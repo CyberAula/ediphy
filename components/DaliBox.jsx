74918c11f5c53756a92331abcc9ed919aab3fac1
Jorge Bellon <j.bellonc@gmail.com>
2015-11-26 17:38:58 +0100

    Removing some magic numbers

--- components/DaliBox.jsx
+++ components/DaliBox.jsx
@@ -2,24 +2,27 @@ import React, {Component} from 'react';
 
 export default class DaliBox extends Component{
     render(){
+        let borderSize = 2;
+        let cornerSize = 15;
+
         let box = this.props.box;
         box.style['position'] = 'absolute';
-        box.style['left'] = 7;
-        box.style['top'] = 7;
+        box.style['left'] = (cornerSize / 2 + borderSize);
+        box.style['top'] = (cornerSize / 2 + borderSize);
 
         return (<div onClick={e => this.handleBoxSelection(this.props.id)}
                      onTouchStart={e => this.handleBoxSelection(this.props.id)}
                      style={{position: 'absolute',
                             left: box.position.x,
                             top: box.position.y,
-                            width: box.width + 14,
-                            height: box.height + 14}}>
+                            width: box.width + (cornerSize + borderSize * 2),
+                            height: box.height + (cornerSize + borderSize * 2)}}>
                     <div style={{visibility: (this.props.isSelected ? 'visible' : 'hidden'), width: '100%', height: '100%'}}>
-                        <div style={{position: 'absolute', left: 0,  top: 0,    width: 10, height: 10, backgroundColor: 'gray'}}></div>
-                        <div style={{position: 'absolute', right: 0, top: 0,    width: 10, height: 10, backgroundColor: 'gray'}}></div>
-                        <div style={{position: 'absolute', left: 0,  bottom: 0, width: 10, height: 10, backgroundColor: 'gray'}}></div>
-                        <div style={{position: 'absolute', right: 0, bottom: 0, width: 10, height: 10, backgroundColor: 'gray'}}></div>
-                        <div style={{position: 'absolute', left: 5, top: 5, width: box.width, height: box.height, border: "2px dashed black"}}></div>
+                        <div style={{position: 'absolute', left: 0,  top: 0,    width: cornerSize, height: cornerSize, backgroundColor: 'gray'}}></div>
+                        <div style={{position: 'absolute', right: 0, top: 0,    width: cornerSize, height: cornerSize, backgroundColor: 'gray'}}></div>
+                        <div style={{position: 'absolute', left: 0,  bottom: 0, width: cornerSize, height: cornerSize, backgroundColor: 'gray'}}></div>
+                        <div style={{position: 'absolute', right: 0, bottom: 0, width: cornerSize, height: cornerSize, backgroundColor: 'gray'}}></div>
+                        <div style={{position: 'absolute', left: (cornerSize / 2), top: (cornerSize / 2), width: box.width, height: box.height, border: (borderSize + "px dashed black")}}></div>
                     </div>
                     <div style={box.style} dangerouslySetInnerHTML={{__html: box.content}}></div>
         </div>);
