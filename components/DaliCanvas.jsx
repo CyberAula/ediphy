37399b9bab4d092170a9797fa5147bf46d106cc2
Jorge Bellon <j.bellonc@gmail.com>
2015-12-15 19:55:19 +0100

    Showing white page only when there is at least one

--- components/DaliCanvas.jsx
+++ components/DaliCanvas.jsx
@@ -6,7 +6,7 @@ export default class DaliCanvas extends Component{
     render(){
         return(
             <div style={{backgroundColor: 'gray', height: '100%', paddingTop: '5%'}}>
-                <div style={{height: '100%', backgroundColor: 'white', margin: '0px 100px 0px 100px'}}>
+                <div style={{height: '100%', backgroundColor: 'white', margin: '0px 100px 0px 100px', visibility: (this.props.showCanvas ? 'visible' : 'hidden')}}>
                     {this.props.ids.map(id =>{
                         let box = this.props.boxes[id];
                         if(box.parent === this.props.pageSelected) {
