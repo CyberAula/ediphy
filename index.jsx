ad846dc52f5dd63420cb2bc8134d66b6966971d5
Jorge Bellon <j.bellonc@gmail.com>
2016-01-11 12:43:42 +0100

    Added redux devtools

--- index.jsx
+++ index.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import { createStore, compose } from 'redux';
 import {Provider} from 'react-redux';
 import DaliApp from './containers/DaliApp';
+import DevTools from './containers/DevTools';
 import GlobalState from './reducers';
+
+const finalCreateStore = compose(
+    // Required! Enable Redux DevTools with the monitors you chose
+    DevTools.instrument()
+)(createStore);
 
 let initialState = {
     present: {navItemsById: {0: {id: 0, children: [], boxes: [], level: 0, type: ''}}}
 };
-let store = createStore(GlobalState, initialState);
+let store = finalCreateStore(GlobalState, initialState);
 
 let root = document.getElementById('root');
 ReactDOM.render(
     <Provider store={store}>
-        <DaliApp />
+        <div style={{height: '100%'}}>
+            <DaliApp />
+            <DevTools />
+        </div>
     </Provider>, root
     );
