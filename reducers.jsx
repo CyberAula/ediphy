d66d132224f8c8eebd41d2c4f2a2ce2be52f9a23
sonsoleslp <sonsoleslp@hotmail.com>
2016-02-12 15:44:18 +0100

    Debugging. Delete Boxes and Toolbars when navIttem is removed

--- reducers.jsx
+++ reducers.jsx
@@ -256,7 +256,7 @@ function recalculateNames(state = {},old = {}, resta = 0, numeroBorrados = 0){
         if(items[section].level == 1){
              items[section].name = 'Section '+(mainindex++);
         } else {
-            var sub = items[items[section].parent].children.indexOf(section)+1
+            var sub = items[items[section].parent].children.filter(s => s[0]=='s').indexOf(section)+1
             items[section].name = items[items[section].parent].name+'.'+ sub;
            
 
