5cf4083d2a759dfdde5c22a4cfee8d45f2fa638d
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2016-02-19 14:36:06 +0100

    Plugin Pills: cambia colores

--- dist/plugins/BasicPills/BasicPills.js
+++ dist/plugins/BasicPills/BasicPills.js
@@ -48,31 +48,41 @@ var BasicPills = (function(){
                 var number = state.number;
                 var auxTitles = state.titles;
                 var auxTexts = state.texts;
+                var auxColors = state.colors;
 
                 for(var i = 0; i < number ; i++){
                // if(i==0){
                 if(auxTitles[i]){
                     console.log(auxTitles[i]);
                 }else{
-                    auxTitles.push('title'+i);}
-                    if(auxTexts[i]){
-                        console.log(auxTitles[i]);
-                    }else{
-                        auxTexts.push('text'+i); }
+                    auxTitles.push('title'+i);
+                }
+                if(auxTexts[i]){
+                    console.log(auxTexts[i]);
+                }else{
+                    auxTexts.push('text'+i); 
+                }
+                if(auxColors[i]){
+                    console.log(auxColors[i]);
+                }else{
+                    auxColors.push('#FFFF00'); 
+                }
+
 
                         editorBox += '\
                         <div class="col-xs-12 text-center"><hr><br><h2>Pill'+i+'</h2></div>\n\
                         <div class="col-xs-12 col-sm-6">Titulo:<input onchange="BasicPills.setTitle('+i+')" type="text" autofocus id="title'+i+'" value= '+ auxTitles[i] +'></div>\n\
-                        <div class="col-xs-12 col-sm-6">Color: <input type="color" name="favcolor" id="color+'+i+'" value="#ff0000"> </div>\n\
-                        <div class="col-xs-12">Texto</div>';
+                        <div class="col-xs-12 col-sm-6">Color: <input onchange="BasicPills.setColor('+i+')" type="color" name="favcolor" id="color'+i+'" value="'+auxColors[i]+'"> </div>\n\
+                        <div class="col-xs-12">'+auxTexts[i]+'</div>';
                     }
 
                     BasicPills.setState('titles',auxTitles);
                     BasicPills.setState('texts',auxTexts);
+                     BasicPills.setState('colors',auxColors);
 
                     //$('#configuradores').html(editorBox);
 
-                return "<div>Número de pestañas <input type=\"number\" onchange='BasicPills.showPreview()' autofocus id=\"BasicPills_input\" value=\"" + state.number + "\"></div><div id=\"fdsf\" class='row'> </div><div id=\"configuradores\" class=\"row\">"+editorBox+"</div>";
+                    return "<div>Número de pestañas <input type=\"number\" onchange='BasicPills.showPreview()' autofocus id=\"BasicPills_input\" value=\"" + state.number + "\"></div><div id=\"fdsf\" class='row'> </div><div id=\"configuradores\" class=\"row\">"+editorBox+"</div>";
             // <button onclick=\"BasicPills.showPreview()\">Show preview</button></div>"
             /*return "<div> Url: 
             <input type=\"text\" autofocus id=\"BasicImage_input\" value=\"" + state.url + "\">
@@ -85,8 +95,10 @@ var BasicPills = (function(){
             var number = state.number;
             var auxTitles = state.titles;
             var auxTexts = state.texts;
+             var auxColors = state.colors;
             console.log(auxTitles);
             console.log(auxTexts);
+            console.log(auxColors);
             var auxNavPills = '';
             var auxTextsPills = '';
 
@@ -100,12 +112,12 @@ var BasicPills = (function(){
                 if(i==0){
                     //auxTitles.push('title'+i); 
                     //auxTexts.push('text'+i); 
-                    auxNavPills += '<div style="padding:0%" class="col-xs-'+ ancho+'"><li style="padding:0%; background-color: red;" class="active" onClick="BasicPills.handleSelect('+i+')">'+auxTitles[i]+'</li></div>';
+                    auxNavPills += '<div style="padding:0%" class="col-xs-'+ ancho+'"><li style="padding:0%; background-color: '+auxColors[i]+';" class="active" onClick="BasicPills.handleSelect('+i+')">'+auxTitles[i]+'</li></div>';
                     auxTextsPills += '<div id="Text'+i+'Pill'+n+'"><p>'+auxTexts[i].toString()+'</p></div>';
                 }else{
                     //auxTitles.push('title'+i); 
                     //auxTexts.push('text'+i); 
-                    auxNavPills +=  '<div class="col-xs-'+ancho+'"><li style="padding:0%"  onClick="BasicPills.handleSelect('+i+')">'+auxTitles[i]+'</li></div>';
+                    auxNavPills +=  '<div class="col-xs-'+ancho+'"><li style="padding:0%;background-color: '+auxColors[i]+';" onClick="BasicPills.handleSelect('+i+')">'+auxTitles[i]+'</li></div>';
                     auxTextsPills += '<div style="display: none" id="Text'+i+'Pill'+n+'" ><p>'+auxTexts[i].toString()+'</p></div>';
                 }
             }
@@ -142,7 +154,7 @@ var BasicPills = (function(){
                             <div class="text-center row">\n\
                             <ul class="nav nav-pills">'+auxNavPills+'</ul>\n\
                             </div>\n\
-                            <div class="row" style="border: red;border-style: solid;border-radius: 0.5em;margin-top: 3%;" >'+auxTextsPills+'</div>\n\
+                            <div class="row" style="border: red;border-style: solid;border-radius: 0.5em;margin-top: 1%;" >'+auxTextsPills+'</div>\n\
                             </div>'
             //"<ul class='nav nav-pills'><li>a</li><li>c</li><li>b</li></ul>"
 
@@ -163,9 +175,10 @@ var BasicPills = (function(){
 
         var number = input.val();
         console.log(state);
-        var auxTitles = [] || state.titles;
-        var auxTexts = [] || state.texts;
-
+        var auxTitles = state.titles;
+        var auxTexts = state.texts;
+        var auxColors = state.colors;
+        console.log("ada"+ state.colors);
         var editorBox = '';
 
         console.log(number);
@@ -174,21 +187,29 @@ var BasicPills = (function(){
                 if(auxTitles[i]){
                     console.log(auxTitles[i]);
                 }else{
-                    auxTitles.push('title'+i);}
-                    if(auxTexts[i]){
-                        console.log(auxTitles[i]);
-                    }else{
-                        auxTexts.push('text'+i); }
+                    auxTitles.push('title'+i);
+                }
+                if(auxTexts[i]){
+                    console.log(auxTitles[i]);
+                }else{
+                    auxTexts.push('text'+i);
+                }
+                if(auxColors[i]){
+                    console.log(auxColors[i]);
+                }else{
+                    auxColors.push('#ff0000');
+                }
 
                         editorBox += '\
                         <div class="col-xs-12 text-center"><hr><br><h2>Pill'+i+'</h2></div>\n\
                         <div class="col-xs-12 col-sm-6">Titulo:<input onchange="BasicPills.setTitle('+i+')" type="text" autofocus id="title'+i+'" value= '+ auxTitles[i] +'></div>\n\
-                        <div class="col-xs-12 col-sm-6">Color: <input type="color" name="favcolor" id="color+'+i+'" value="#ff0000"> </div>\n\
+                        <div class="col-xs-12 col-sm-6">Color: <input onchange="BasicPills.setColor('+i+')" type="color" name="favcolor" id="color'+i+'" value="'+auxColors[i]+'"> </div>\n\
                         <div class="col-xs-12">Texto</div>';
                     }
 
                     BasicPills.setState('titles',auxTitles);
                     BasicPills.setState('texts',auxTexts);
+                    BasicPills.setState('colors',auxColors);
 
                     $('#configuradores').html(editorBox);
                 },
@@ -199,6 +220,16 @@ var BasicPills = (function(){
                     auxTitles = state.titles;
                     auxTitles[id] = input.val();
                     BasicPills.setState('titles',auxTitles);
+                },
+                setColor: function(id){
+                    var state = BasicPills.getState();
+                    var idT = '#color'+id;
+                    var input = $(idT);
+                    console.log(input.val())
+                    auxColors = state.colors;
+                    auxColors[id] = input.val();
+                    console.log(auxColors[id]);
+                    BasicPills.setState('colors',auxColors);
                 },
                 imageClick: function() {
                     alert("Miau!");
