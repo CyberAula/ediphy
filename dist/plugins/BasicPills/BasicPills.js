ccf7be2e783df26879c7d8ae9e2ce3e289370979
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2016-02-22 13:57:04 +0100

    CKeditor incluido

--- dist/plugins/BasicPills/BasicPills.js
+++ dist/plugins/BasicPills/BasicPills.js
@@ -40,30 +40,32 @@ var BasicPills = (function(){
                 return {number: 0, titles: [], texts: [], colors: []};
             },
             getConfigTemplate: function(state){
-                console.log('EDITANdo');
-                console.log(state);
-                console.log(BasicPills.getState());
+                //console.log('EDITANdo');
+                //console.log(state);
+                //console.log(BasicPills.getState());
 
                 var editorBox = '';
                 var number = state.number;
                 var auxTitles = state.titles;
                 var auxTexts = state.texts;
                 var auxColors = state.colors;
+                var d = new Date();
+                var n = d.getTime();
 
                 for(var i = 0; i < number ; i++){
                // if(i==0){
                 if(auxTitles[i]){
-                    console.log(auxTitles[i]);
+                    //console.log(auxTitles[i]);
                 }else{
                     auxTitles.push('title'+i);
                 }
                 if(auxTexts[i]){
-                    console.log(auxTexts[i]);
+                    //console.log(auxTexts[i]);
                 }else{
                     auxTexts.push('text'+i); 
                 }
                 if(auxColors[i]){
-                    console.log(auxColors[i]);
+                    //console.log(auxColors[i]);
                 }else{
                     auxColors.push('#FFFF00'); 
                 }
@@ -71,18 +73,38 @@ var BasicPills = (function(){
 
                         editorBox += '\
                         <div class="col-xs-12 text-center"><hr><br><h2>Pill'+i+'</h2></div>\n\
-                        <div class="col-xs-12 col-sm-6">Titulo:<input onchange="BasicPills.setTitle('+i+')" type="text" autofocus id="title'+i+'" value= '+ auxTitles[i] +'></div>\n\
-                        <div class="col-xs-12 col-sm-6">Color: <input onchange="BasicPills.setColor('+i+')" type="color" name="favcolor" id="color'+i+'" value="'+auxColors[i]+'"> </div>\n\
-                        <div class="col-xs-12">'+auxTexts[i]+'</div>';
+                        <div class="col-xs-12 col-sm-6">Titulo:\n\
+                          <input onchange="BasicPills.setTitle('+i+')" type="text" autofocus id="title'+i+'" value= '+ auxTitles[i] +'></div>\n\
+                        <div class="col-xs-12 col-sm-6">Color:\n\
+                          <input onchange="BasicPills.setColor('+i+')" type="color" name="favcolor" id="color'+i+'" value="'+auxColors[i]+'"> </div>\n\
+                        <div class="col-xs-12 well textEditable" onfocus="BasicPills.ponerCKEditor('+i+','+n+')" onblur="BasicPills.setText('+i+')" contentEditable id="text'+i+'">'+auxTexts[i]+'</div>';
+                        /*
+                         <script>\n\
+                          CKEDITOR.disableAutoInline = true;\n\
+                          var editId = "text'+i+'n'+n+'editor";\n\
+                          console.log(editId);
+                          var cajaEdit = document.getElementById(editId);\n\
+                          CKEDITOR.inline(cajaEdit);\n\
+                        </script>';*/
+
+                        //BasicPills.ponerCKEditor(i,n);
+
                     }
 
                     BasicPills.setState('titles',auxTitles);
                     BasicPills.setState('texts',auxTexts);
                      BasicPills.setState('colors',auxColors);
-
+//onclick="BasicPills.ponerCKEditor('+i+','+n+')" onkeyup="BasicPills.setText('+i+')" 
                     //$('#configuradores').html(editorBox);
 
-                    return "<div>Número de pestañas <input type=\"number\" onchange='BasicPills.showPreview()' autofocus id=\"BasicPills_input\" value=\"" + state.number + "\"></div><div id=\"fdsf\" class='row'> </div><div id=\"configuradores\" class=\"row\">"+editorBox+"</div>";
+
+
+                    return "\
+                    <div>Número de pestañas \n\
+                      <input type=\"number\" onchange='BasicPills.showPreview()' autofocus id=\"BasicPills_input\" value=\"" + state.number + "\">\n\
+                    </div>\n\
+                    <div id=\"fdsf\" class='row'></div>\n\
+                    <div id=\"configuradores\" class=\"row\">"+editorBox+"</div>";
             // <button onclick=\"BasicPills.showPreview()\">Show preview</button></div>"
             /*return "<div> Url: 
             <input type=\"text\" autofocus id=\"BasicImage_input\" value=\"" + state.url + "\">
@@ -96,32 +118,58 @@ var BasicPills = (function(){
             var auxTitles = state.titles;
             var auxTexts = state.texts;
              var auxColors = state.colors;
-            console.log(auxTitles);
-            console.log(auxTexts);
-            console.log(auxColors);
+            //console.log(auxTitles);
+            //console.log(auxTexts);
+            //console.log(auxColors);
             var auxNavPills = '';
             var auxTextsPills = '';
 
             var ancho = 12/number;
             var d = new Date();
             var n = d.getTime();
-
+            var resto = 12%number;
+            //console.log(resto);
 
 
             for(var i = 0; i < number ; i++){
+
+
                 if(i==0){
                     //auxTitles.push('title'+i); 
                     //auxTexts.push('text'+i); 
-                    auxNavPills += '<div style="padding:0%" class="col-xs-'+ ancho+'"><li style="padding:0%; background-color: '+auxColors[i]+';" class="active" onClick="BasicPills.handleSelect('+i+')">'+auxTitles[i]+'</li></div>';
-                    auxTextsPills += '<div id="Text'+i+'Pill'+n+'"><p>'+auxTexts[i].toString()+'</p></div>';
+                    auxNavPills += '\
+                        <li id="li'+n+'" style="padding:0%; background-color: '+auxColors[i]+';"\n\
+                          class="col-xs-'+ ancho+' active collapsed"\n\
+                          aria-controls="Text'+i+'Pill'+n+'" href="#Text'+i+'Pill'+n+'"\n\
+                          aria-expanded="true" data-toggle="collapse" role="button"\n\
+                          onClick="BasicPills.handleSelect('+i+','+n+','+number+')">'+auxTitles[i]+'</li>';
+
+                    auxTextsPills += '\
+                        <div class="Pill'+n+' row collapse" id="Text'+i+'Pill'+n+'"\n\
+                          style="border: red;border-style: solid;border-radius: 0.5em;margin-top: 1%;" >\n\
+                            <p>'+auxTexts[i]+'</p>\n\
+                        </div>';
                 }else{
                     //auxTitles.push('title'+i); 
                     //auxTexts.push('text'+i); 
-                    auxNavPills +=  '<div class="col-xs-'+ancho+'"><li style="padding:0%;background-color: '+auxColors[i]+';" onClick="BasicPills.handleSelect('+i+')">'+auxTitles[i]+'</li></div>';
-                    auxTextsPills += '<div style="display: none" id="Text'+i+'Pill'+n+'" ><p>'+auxTexts[i].toString()+'</p></div>';
+                    auxNavPills +=  '\
+                        <li class="col-xs-'+ancho+' collapsed"\n\
+                          aria-controls="Text'+i+'Pill'+n+'" href="#Text'+i+'Pill'+n+'"\n\
+                          aria-expanded="false" data-toggle="collapse" role="button"\n\
+                          style="padding:0%;background-color: '+auxColors[i]+';margin: 0%"\n\
+                          onClick="BasicPills.handleSelect('+i+','+n+','+number+')">'+auxTitles[i]+'</li>';
+                    auxTextsPills += '\
+                        <div class="Pill'+n+' row collapse" id="Text'+i+'Pill'+n+'"\n\
+                          style="border: red;border-style: solid;border-radius: 0.5em;margin-top: 1%" >\n\
+                            <p>'+auxTexts[i]+'</p>\n\
+                        </div>';
                 }
             }
-
+/*
+            if(12%number > 0 ){
+                console.log('impar');
+                $('#li'+n).addClass('col-xs-offset-1');
+            }*/
             //BasicPills.setState('titles',auxTitles);
             //BasicPills.setState('texts',auxTexts);
 
@@ -152,87 +200,122 @@ var BasicPills = (function(){
                             <div style="padding: 0% 5% 0% 5%">\n\
                             </br>\n\
                             <div class="text-center row">\n\
-                            <ul class="nav nav-pills">'+auxNavPills+'</ul>\n\
-                            </div>\n\
-                            <div class="row" style="border: red;border-style: solid;border-radius: 0.5em;margin-top: 1%;" >'+auxTextsPills+'</div>\n\
-                            </div>'
+                            <ul class="nav nav-pills nav-justified">'+auxNavPills+'</ul>\n\
+                            </div>'+auxTextsPills+'</div>'
             //"<ul class='nav nav-pills'><li>a</li><li>c</li><li>b</li></ul>"
 
 
             //<Nav className='pills' activeKey={1} onSelect={BasicPills.handleSelect()}><NavItem eventKey={1} href='/home'>NavItem 1 content</NavItem><NavItem eventKey={2} title='Item'>NavItem 2 content</NavItem><NavItem eventKey={3} disabled>NavItem 3 content</NavItem></Nav>";
         },
-        handleSelect: function (selectedKey) {
-          alert('selected ' + selectedKey);
-      },
-      handleToolbar: function(name, value){
-        if(name === 'borderSize')
-            BasicImage.setState('borderSize', value);
-    },
-    showPreview: function(){
-        var state = BasicPills.getState();
-        var input = $('#BasicPills_input');
-        BasicPills.setState('number',input.val());
+        handleSelect: function (selectedKey, idTime, number) {
+            var clase = '.Pill'+idTime;
+            $(id).css('display','block');
 
-        var number = input.val();
-        console.log(state);
-        var auxTitles = state.titles;
-        var auxTexts = state.texts;
-        var auxColors = state.colors;
-        console.log("ada"+ state.colors);
-        var editorBox = '';
+            for(var i = 0; i < number; i++){
+                var id = '#Text'+i+'Pill'+idTime;
+                if(i != selectedKey){
+                    var id = '#Text'+i+'Pill'+idTime;
+                    //console.log(id);
+                    $(id).css('display','none');
+                }else{
+                    $(id).css('display','block');
+                }
+            }
+            /*var clase = '.Pill'+idTime;
+            var id = '#Text'+selectedKey+'Pill'+idTime;
+            $(clase).css('display','none');
+            $(id).css('display','block');*/
+          //alert('selected ' + selectedKey);
+        },
+        handleToolbar: function(name, value){
+            if(name === 'borderSize')
+                BasicImage.setState('borderSize', value);
+        },
+        showPreview: function(){
+            var state = BasicPills.getState();
+            var input = $('#BasicPills_input');
+            BasicPills.setState('number',input.val());
 
-        console.log(number);
-        for(var i = 0; i < number ; i++){
-               // if(i==0){
-                if(auxTitles[i]){
-                    console.log(auxTitles[i]);
-                }else{
-                    auxTitles.push('title'+i);
-                }
-                if(auxTexts[i]){
-                    console.log(auxTitles[i]);
-                }else{
-                    auxTexts.push('text'+i);
-                }
-                if(auxColors[i]){
-                    console.log(auxColors[i]);
-                }else{
-                    auxColors.push('#ff0000');
-                }
+            var number = input.val();
+            //console.log(state);
+            var auxTitles = state.titles;
+            var auxTexts = state.texts;
+            var auxColors = state.colors;
+            //console.log("ada"+ state.colors);
+            var editorBox = '';
 
-                        editorBox += '\
-                        <div class="col-xs-12 text-center"><hr><br><h2>Pill'+i+'</h2></div>\n\
-                        <div class="col-xs-12 col-sm-6">Titulo:<input onchange="BasicPills.setTitle('+i+')" type="text" autofocus id="title'+i+'" value= '+ auxTitles[i] +'></div>\n\
-                        <div class="col-xs-12 col-sm-6">Color: <input onchange="BasicPills.setColor('+i+')" type="color" name="favcolor" id="color'+i+'" value="'+auxColors[i]+'"> </div>\n\
-                        <div class="col-xs-12">Texto</div>';
+            //console.log(number);
+            for(var i = 0; i < number ; i++){
+                   // if(i==0){
+                    if(auxTitles[i]){
+                        //console.log(auxTitles[i]);
+                    }else{
+                        auxTitles.push('title'+i);
+                    }
+                    if(auxTexts[i]){
+                        //console.log(auxTitles[i]);
+                    }else{
+                        auxTexts.push('text'+i);
+                    }
+                    if(auxColors[i]){
+                        //console.log(auxColors[i]);
+                    }else{
+                        auxColors.push('#ff0000');
                     }
 
-                    BasicPills.setState('titles',auxTitles);
-                    BasicPills.setState('texts',auxTexts);
-                    BasicPills.setState('colors',auxColors);
+                            editorBox += '\
+                            <div class="col-xs-12 text-center"><hr><br><h2>Pill'+i+'</h2></div>\n\
+                            <div class="col-xs-12 col-sm-6">Titulo:<input onchange="BasicPills.setTitle('+i+')" type="text" autofocus id="title'+i+'" value= '+ auxTitles[i] +'></div>\n\
+                            <div class="col-xs-12 col-sm-6">Color: <input onchange="BasicPills.setColor('+i+')" type="color" name="favcolor" id="color'+i+'" value="'+auxColors[i]+'"> </div>\n\
+                            <div class="col-xs-12">Texto</div>';
+                        }
 
-                    $('#configuradores').html(editorBox);
-                },
-                setTitle: function(id){
-                    var state = BasicPills.getState();
-                    var idT = '#title'+id;
-                    var input = $(idT);
-                    auxTitles = state.titles;
-                    auxTitles[id] = input.val();
-                    BasicPills.setState('titles',auxTitles);
-                },
-                setColor: function(id){
-                    var state = BasicPills.getState();
-                    var idT = '#color'+id;
-                    var input = $(idT);
-                    console.log(input.val())
-                    auxColors = state.colors;
-                    auxColors[id] = input.val();
-                    console.log(auxColors[id]);
-                    BasicPills.setState('colors',auxColors);
-                },
-                imageClick: function() {
-                    alert("Miau!");
-                }
-            });
-})();
+                        BasicPills.setState('titles',auxTitles);
+                        BasicPills.setState('texts',auxTexts);
+                        BasicPills.setState('colors',auxColors);
+
+                        $('#configuradores').html(editorBox);
+                    },
+                    setTitle: function(id){
+                        var state = BasicPills.getState();
+                        var idT = '#title'+id;
+                        var input = $(idT);
+                        auxTitles = state.titles;
+                        auxTitles[id] = input.val();
+                        BasicPills.setState('titles',auxTitles);
+                    },
+                      setText: function(id){
+                        //alert("Hola");
+                        //console.log("texto");
+                        var state = BasicPills.getState();
+
+                        var idT = 'text'+id;
+                        var input = document.getElementById(idT).innerHTML;
+                        auxTexts = state.texts;
+                        auxTexts[id] = input;
+                        console.log(input);
+                       // BasicPills.setState('texts',auxTexts);
+                    },
+                    setColor: function(id){
+                        var state = BasicPills.getState();
+                        var idT = '#color'+id;
+                        var input = $(idT);
+                        //console.log(input.val())
+                        auxColors = state.colors;
+                        auxColors[id] = input.val();
+                        //console.log(auxColors[id]);
+                        BasicPills.setState('colors',auxColors);
+                    },
+                    imageClick: function() {
+                        alert("Miau!");
+                    },
+                    ponerCKEditor: function(id,time){
+                        CKEDITOR.disableAutoInline = true;
+                        var editId = 'text'+id;
+                        //+'n'+time+'editor';
+                        var cajaEdit = document.getElementById(editId);
+                        console.log(cajaEdit);
+                        CKEDITOR.inline(cajaEdit);
+                    }
+                });
+    })();
