3dc77ca6274b070264cdd6e5ccd0ec84adf9dc2c
Jorge Bellon <j.bellonc@gmail.com>
2016-02-24 15:24:20 +0100

    Adding function selector and removing unnecessary ckeditor files

--- dist/lib/ckeditor/config.js
+++ dist/lib/ckeditor/config.js
@@ -1,39 +1,4 @@
-/**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
- */
-
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
-
-	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
-	];
-
-	// Remove some buttons provided by the standard plugins, which are
-	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
-
-	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
-
-	// Simplify the dialog windows.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
-	config.extraPlugins = 'sourcedialog';
-};
+﻿CKEDITOR.editorConfig=function(a){a.toolbarGroups=[{name:"document",groups:["document","mode","doctools"]},{name:"clipboard",groups:["clipboard","undo"]},{name:"editing",groups:["find","selection","spellchecker","editing"]},{name:"forms",groups:["forms"]},{name:"basicstyles",groups:["basicstyles","cleanup"]},{name:"links",groups:["links"]},{name:"insert",groups:["insert"]},"/",{name:"paragraph",groups:"list indent blocks align bidi paragraph".split(" ")},{name:"styles",groups:["styles"]},{name:"colors",
+groups:["colors"]},{name:"tools",groups:["tools"]},{name:"others",groups:["others"]},{name:"about",groups:["about"]}];a.skin="bootstrapck";a.linkJavaScriptLinksAllowed=!0;a.linkShowAdvancedTab=!1;a.dialog_noConfirmCancel=!0;a.stylesSet=[{name:"Negrita",element:"strong"},{name:"Cursiva",element:"em"},{name:"Enlace web",element:"a",attributes:{"class":"enlaceWeb"}},{name:"Enlace blanco",element:"a",attributes:{"class":"enlaceBlanco"}},{name:"Enlace PDF",element:"a",attributes:{"class":"enlacePdf"}},{name:"Enlace Word",element:"a",
+attributes:{"class":"enlaceWord"}},{name:"Enlace Excel",element:"a",attributes:{"class":"enlaceExcel"}},{name:"Enlace glosario",element:"a",attributes:{"class":"enlaceGlosario"}},{name:"Enlace ZIP",element:"a",attributes:{"class":"enlaceZip"}},{name:"Enlace capa",element:"a",attributes:{"class":"enlaceCapa"}},{name:"Enlace popup",element:"a",attributes:{"class":"enlacePopup"}},{name:"Enlace popup blanco",element:"a",attributes:{"class":"enlacePopup_blanco"}}];a.removeButtons="Source,Templates,Save,NewPage,Preview,Print,Replace,Find,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Bold,Italic,Underline,Strike,Subscript,Superscript,RemoveFormat,NumberedList,BulletedList,Blockquote,CreateDiv,BidiLtr,BidiRtl,Language,Anchor,Flash,Table,HorizontalRule,Smiley,PageBreak,Iframe,SpecialChar,About,Maximize,ShowBlocks,TextColor,BGColor,Format,Font,FontSize,Unlink";
+a.plugins="about,a11yhelp,basicstyles,bidi,blockquote,clipboard,colorbutton,colordialog,contextmenu,dialogadvtab,div,elementspath,enterkey,entities,filebrowser,find,flash,floatingspace,font,format,forms,horizontalrule,htmlwriter,image,iframe,indentlist,indentblock,justify,language,link,list,liststyle,magicline,maximize,newpage,pagebreak,pastefromword,pastetext,preview,print,removeformat,resize,save,selectall,showblocks,showborders,smiley,sourcearea,specialchar,stylescombo,tab,table,tabletools,templates,toolbar,undo,wysiwygarea"};
