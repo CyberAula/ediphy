35fd455e62128efac913478de626881da26bec6f
sonsoleslp <poynting.slp@gmail.com>
2016-04-28 17:05:28 +0200

    Fix export except DaliBoxSortable

--- dist/lib/visor/visor.js
+++ dist/lib/visor/visor.js
@@ -19,8 +19,9 @@ var DaliVisor = {
 
             var zip = new JSZip(data);
             var navs = state.present.navItemsById;
+    
             state.present.navItemsIds.map(page => {
-
+                    
                 var inner = new EJS({url: '/lib/visor/index.ejs'}).render({
                  page: page, 
                  navs: navs,                
@@ -41,27 +42,7 @@ var DaliVisor = {
 
            
           
-    },
-    parseBox:function(box){
-
-        var width = box.width[box.width.length -1]=='%' ? box.width : box.width+'px';
-        var height = box.height? (box.height[box.height.length -1]=='%' ? box.height : box.height+'px'):'auto';
-        var code = '\n<div style="width:'+ width+'; height:'+height+'; position: absolute; top: '+box.position.y+'px; left: '+box.position.x+'px; " >'
-        if (box.id[1]!='s'){
-            code+=box.content
-        }
-
-        box.children.map(b=>{
-            var nn = b; 
-            if (box.id[1]='s') nn= b.replace('sc-','bo-');// Cambiar sc por bo en sortables
-            var alturadiv = alltheboxes[nn].height
-            var height2 = alturadiv? (alturadiv[alturadiv.length -1]=='%' ? alturadiv : alturadiv+'px'):'auto';
-            code+= '<div style="width:100%;position:relative;height:'+height2+';display:block;">' +DaliVisor.parseBox(alltheboxes[nn])+'</div>';
-        });
-        code+= '</div>'
-        return code;
-
-    }
+    } 
 
 
 
