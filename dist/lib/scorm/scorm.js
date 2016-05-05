9c9bef7dfcf52281e3cec9176799c76d1ba249a7
berto_abc <bertocode@gmail.com>
2016-05-05 13:38:38 +0200

    SCORM: It does work, fix bug

--- dist/lib/scorm/scorm.js
+++ dist/lib/scorm/scorm.js
@@ -4,7 +4,7 @@ var DaliScorm = {
 
 
     exports: function(state){
-        
+
         var today = new Date();
         var strDate = 'd-m-Y'
           .replace('d', today.getDate())
@@ -12,7 +12,7 @@ var DaliScorm = {
           .replace('Y', today.getFullYear());
 
         JSZipUtils.getBinaryContent('/lib/scorm/scorm.zip', function(err, data) {
-         
+
             if(err) {
                 throw err; // or handle err
             }
@@ -21,19 +21,19 @@ var DaliScorm = {
             var navs = state.present.navItemsById;
             var sections = [];
             state.present.navItemsIds.map(page => {
-                    
+
                 var inner = new EJS({url: '/lib/visor/index.ejs'}).render({
-                 page: page, 
-                 navs: navs,                
-                 boxesById: state.present.boxesById, 
-                 boxes: state.present.boxes, 
+                 page: page,
+                 navs: navs,
+                 boxesById: state.present.boxesById,
+                 boxes: state.present.boxes,
                  toolbarsById: state.present.toolbarsById,
                  strDate: strDate
                 });
 
                 var nombre = navs[page].name;
                 sections.push(nombre);
-  
+
                 zip.file(nombre+".html", inner);
             });
 
@@ -44,12 +44,12 @@ var DaliScorm = {
             var content = zip.generate({type:"blob"});
             saveAs(content, "scorm.zip");
 
-         
+
 
         });
 
-           
-          
+
+
     } ,
 
 
@@ -62,7 +62,7 @@ var DaliScorm = {
       manifest.setAttribute("xmlns:adlcp","http://www.adlnet.org/xsd/adlcp_rootv1p2" );
       manifest.setAttribute("xmlns:xsi","http://www.w3.org/2001/XMLSchema-instance");
       manifest.setAttribute("identifier", "com.dali.presentation");
-      manifest.setAttribute("version","1");
+      manifest.setAttribute("version","1.0");
       manifest.setAttribute("xsi:schemaLocation", "http://www.imsproject.org/xsd/imscp_rootv1p1p2 imscp_rootv1p1p2.xsd http://www.imsglobal.org/xsd/imsmd_rootv1p2p1 imsmd_rootv1p2p1.xsd http://www.adlnet.org/xsd/adlcp_rootv1p2 adlcp_rootv1p2.xsd");
 
       ///      METADATA
@@ -71,28 +71,28 @@ var DaliScorm = {
       schema_txt = doc.createTextNode("ADL SCORM");
       schema.appendChild(schema_txt);
       metadata.appendChild(schema);
-      
-      var schemaVersion = doc.createElement("schemaVersion");
+
+      var schemaVersion = doc.createElement("schemaversion");
       schema_version_txt = doc.createTextNode("1.2");
-      schemaVersion.appendChild(schema_version_txt); 
+      schemaVersion.appendChild(schema_version_txt);
       metadata.appendChild(schemaVersion);
 
 
       ///       ORGANIZATION
       var organizations = doc.createElement("organizations");
-      organizations.setAttribute("default","default_org");
+      organizations.setAttribute("default","defaultOrganization");
 
       var organization = doc.createElement("organization");
-      organization.setAttribute("identifier","default_org");
+      organization.setAttribute("identifier","defaultOrganization");
 
 
       var title_org = doc.createElement("title");
       title_item_txt = doc.createTextNode(title);
-      title_org.appendChild(title_item_txt);//TODO: Include Title
+      title_org.appendChild(title_item_txt);  //TODO: Include Title
 
       var item_org = doc.createElement("item");
-      item_org.setAttribute("identifier", "item_1");
-      item_org.setAttribute("identifierref","resource_1");
+      item_org.setAttribute("identifier", "item1");
+      item_org.setAttribute("identifierref","resource1");
 
       var title_item = doc.createElement("item");
       title_item_txt = doc.createTextNode("Default Title");
@@ -112,10 +112,10 @@ var DaliScorm = {
       for(var i = 0; i< sections.length; i++){
         //TODO: Iterate over html elements and add this pieze of code
         var resource = doc.createElement("resource");
-        resource.setAttribute("identifier", "resource_"+(i+1)); 
+        resource.setAttribute("identifier", "resource"+(i+1));
         resource.setAttribute("type", "webcontent");
         resource.setAttribute("adlcp:scormtype", "sco");
-        resource.setAttribute("href", sections[i]+".html"); 
+        resource.setAttribute("href", sections[i]+".html");
 
         var file = doc.createElement("file");
         file.setAttribute("href", sections[i]+".html");
