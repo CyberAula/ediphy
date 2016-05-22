9cf02f77a33966831275931b7492a3ed30f6b43d
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2016-05-22 20:19:41 +0200

    onclick ok

--- dist/plugins/CajasColorBis/visor/CajasColorBis.js
+++ dist/plugins/CajasColorBis/visor/CajasColorBis.js
@@ -1,7 +1,7 @@
 Dali.Visor.Plugins["CajasColorBis"] = function (){
     return {
         getRenderTemplate: function (state) {
-            var template = "<div className='cajascolor'>";
+            var template = "<div class='cajascolor'>";
             var disp = 'block';//none
 
             if(state.image){
@@ -14,20 +14,20 @@ Dali.Visor.Plugins["CajasColorBis"] = function (){
             }
 
             if(state.wayHorizontal){
-                template += "<div className='tabla_colores'><div className='fila_colores'>";
+                template += "<div class='tabla_colores'><div class='fila_colores'>";
                 var width = 100 / state.nBoxes;
                 var bloqId = '';
                 for( var i = 0; i<state.nBoxes; i++){
                     bloqId = 'bloque'+i;
-                    template += "<div className='celda_colores "+state.colors[i]+" "+rounded+"'  onclick='$dali$.showDiv(i)' style='height: 3em; width: " + width + "%'><plugin plugin-data-key='title" + i + "' plugin-data-default='BasicText' /></div>";
+                    template += "<div class='celda_colores "+state.colors[i]+" "+rounded+"'  onclick='$dali$.showDiv("+i+")' style='height: 3em; width: " + width + "%'><plugin plugin-data-key='title" + i + "' plugin-data-default='BasicText' /></div>";
                     if(i !== (state.nBoxes -1)){
-                        template += "<div className='sep'></div>";
+                        template += "<div class='sep'></div>";
                     }
                 }
                  template += "</div></div>"
 
                 for( var i = 0; i<state.nBoxes; i++){
-                      template += "<div id='bloque"+i+"' className='bloque_colores capa_"+state.colors[i]+" "+rounded+"'  style='min-height: 80px; height: 1px; display:"+disp+"'><plugin plugin-data-key='box" + i + "' /></div>";
+                      template += "<div id='bloque"+i+"' class='bloque_colores capa_"+state.colors[i]+" "+rounded+"'  style='min-height: 80px; height: 1px; display:"+disp+"'><plugin plugin-data-key='box" + i + "' /></div>";
                       if(i==0){
                         disp = 'none';
                       }
@@ -37,10 +37,10 @@ Dali.Visor.Plugins["CajasColorBis"] = function (){
                 var bloqId = '';
                 for( var i = 0; i<state.nBoxes; i++){
                      bloqId = 'bloque'+i;;
-                    template += "<div className='tabla_colores'><div className='fila_colores'>";
-                    template += "<div className='celda_colores "+state.colors[i]+" "+rounded+"'   onclick='$dali$.showDiv(i)'style='height: 3em'><plugin plugin-data-key='title" + i + "' plugin-data-default='BasicText' /></div>";
+                    template += "<div class='tabla_colores'><div class='fila_colores'>";
+                    template += "<div class='celda_colores "+state.colors[i]+" "+rounded+"'   onclick='$dali$.showDiv("+i+")' style='height: 3em'><plugin plugin-data-key='title" + i + "' plugin-data-default='BasicText' /></div>";
                     template += "</div></div>"
-                    template += "<div id='bloque"+i+"' className='bloque_colores capa_"+state.colors[i]+" "+rounded+"'  style='min-height: 80px; height: 1px; display:"+disp+"'><plugin plugin-data-key='box" + i + "' /></div>";
+                    template += "<div id='bloque"+i+"' class='bloque_colores capa_"+state.colors[i]+" "+rounded+"'  style='min-height: 80px; height: 1px; display:"+disp+"'><plugin plugin-data-key='box" + i + "' /></div>";
                        if(i==0){
                         disp = 'none';
                       } 
@@ -53,18 +53,7 @@ Dali.Visor.Plugins["CajasColorBis"] = function (){
         },
         showDiv: function(idDiv)
         {
-            var id = '#bloque'+idDiv;
-            $(id).slideToggle("slow",function);
-
-           /*function showDiv(_div){
- +    _div.fadeTo('slow',1, function() {});
- +}
- +function hideDiv(_div){
- +    _div.fadeTo('slow',0, function() {
- +        $(this).empty();
- +        $(this).remove();
- +    });
- +}*/
+            alert(idDiv);
         }
     }
 }
