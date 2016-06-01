8ffd17abc76e8921fc69be9c7a13cf8c5900d9ed
ramirobg94 <ramiro.blazquezgon@alumnos.upm.es>
2016-06-01 14:38:15 +0200

    toolbar and colors in CajasColorBis works

--- dist/plugins/CajasColorBis/CajasColorBis.js
+++ dist/plugins/CajasColorBis/CajasColorBis.js
@@ -14,39 +14,17 @@ Dali.Plugins["CajasColorBis"] = function (base){
                 main: {
                     __name: "Main",
                     accordions:{
-                        pepe: {
-                            __name: "sub",
-                            buttons: {},
-                            accordions:{
-                                juan:{
-                                    __name: "bravo",
-                                    buttons:{
-                                        __name: "subAcr",
-                                        type: 'number',
-                                        value: 2,
-                                        max: 8,
-                                        min: 1,
-                                        autoManaged: false
-                                    }
-                                }
-                            }   
-                        },
-                        number: {
-                            __name: "Number",
+                        general: {
+                            __name: "General",
                             buttons:{
-                                nBoxes:{
+                                 nBoxes:{
                                     __name: "Number of boxes",
                                     type: 'number',
                                     value: 2,
                                     max: 8,
                                     min: 1,
                                     autoManaged: false
-                                }
-                            }
-                        },
-                        style: {
-                            __name: "Estilo CajasColor",
-                            buttons:{
+                                },
                                 wayHorizontal:{
                                     __name: "Dirección",
                                     type: 'number',
@@ -72,7 +50,7 @@ Dali.Plugins["CajasColorBis"] = function (base){
                                     autoManaged: false 
                                 }
                             }
-                        }
+                        }//,
                     }
                 }
             }
@@ -84,24 +62,24 @@ Dali.Plugins["CajasColorBis"] = function (base){
            toolBar.main.accordions.buttonStyle = {__name: "Estilo botones", buttons: {}};
            for(var i = 0; i < base.getState().nBoxes; i++){
                 toolBar.main.accordions.buttonStyle.buttons["box" + i] = {
-                    __name: "caja" + i,
-                    type: "text",
-                    value: "aa",
-                     isAttribute: true,
-                     autoManaged: false 
+                __name: 'caja'+i,
+                                    type: 'select',
+                                    value: 'verdeoscuro',
+                                    options: ['verdeoscuro', 'cyan', 'granate', 'naranja', 'rojo', 'azul', 'marron', 'rojizo', 'azulpuro', 'azulverdoso', 'violeta', 'marronvivo','gris','amarillo'],
+                                    autoManaged: false
                 }
            }
-console.log(toolBar);
+            console.log(toolBar);
             return toolBar;
         },
         getInitialState: function(){//el color de las cajas es el capa_{colors[i]}
             return {nBoxes: 2, colors: ['azulverdoso', 'azulpuro'], wayHorizontal: true, image: true, rounded: false};
         },
         getRenderTemplate: function(state){
-            var template = "<div class='cajascolor'>";
+            var template = "<div class='cajascolor' style='width: 100%; height: 100%'>";
             var disp = 'block';
             if(state.image){
-                template += "<div style='height: 100%'><plugin plugin-data-key='image' plugin-data-default='BasicImage' /></div>";
+                template += "<div style='height: 20%; max-height: 100px;' ><plugin plugin-data-key='image' plugin-data-default='BasicImage' /></div>";
             }
 
             var rounded = '';
@@ -121,7 +99,7 @@ console.log(toolBar);
                  template += "</div></div>"
 
                 for( var i = 0; i<state.nBoxes; i++){
-                       template += "<div class='bloque_colores capa_"+state.colors[i]+" "+rounded+"'  style='min-height: 80px; height: 1px; display:"+disp+"'><plugin plugin-data-key='box" + i + "' /></div>";
+                       template += "<div class='bloque_colores capa_"+state.colors[i]+" "+rounded+"'  style='min-height: 80px; height: 25%  !important; display:"+disp+"'><plugin plugin-data-key='box" + i + "' /></div>";
                 }
 
             }else{
@@ -129,58 +107,58 @@ console.log(toolBar);
                     template += "<div class='tabla_colores'><div class='fila_colores'>";
                     template += "<div class='celda_colores "+state.colors[i]+" "+rounded+"'   onclick='$dali$.click()' style='height: 3em'><plugin plugin-data-key='title" + i + "' plugin-data-default='BasicText' /></div>";
                     template += "</div></div>"
-                      template += "<div class='bloque_colores capa_"+state.colors[i]+" "+rounded+"'  style='min-height: 80px; height: 1px; display:"+disp+"'><plugin plugin-data-key='box" + i + "' /></div>";
+                      template += "<div class='bloque_colores capa_"+state.colors[i]+" "+rounded+"'  style='min-height: 80px; height: 25% !important; display:"+disp+"'><plugin plugin-data-key='box" + i + "' /></div>";
                 }
             }
 
-            template += "</div>"
+            template += "</div></div>"
 
             return template;
         },
         handleToolbar: function(name, value){
-            console.log(value);
-            switch(name){
-                case 'nBoxes':
 
-                    var diff = value - base.getState().nBoxes;
-                    diff = Math.abs(diff);
-
-                    if(value > base.getState().nBoxes){
-                        do{
-                            base.setState('colors', base.getState().colors.concat(['azul']));
-                            diff--;
-                        }while(diff > 0)
-                    }else if(value < base.getState().nBoxes){
-                        var newColors = base.getState().colors;
-                        
-                          do{
-                            newColors.pop();
-                            base.setState('colors', newColors);
-                            diff--;
-                        }while(diff > 0)
-
-                    }
-                    base.setState(name, value);
-                    break;
-                case 'wayHorizontal':
-                case 'image':
-                case 'rounded':
-                    base.setState(name, !!value);
-                    break;
-                case /box/.test(name):
+            if( /box/.test(name) ){
                     console.log(name);
-                    var idB = parseInt(name.slice(4));
+                    console.log('hello');
+                    console.log(name.slice(0));
+                    console.log(name.slice(1));
+                    console.log(name.slice(2));
+                    console.log(name.slice(3));
+                    var idB = name.slice(3);
+                    console.log(idB);
                     var newColors = base.getState().colors;
                     newColors[idB] = value;
+                    console.log("newColors");
                     console.log(newColors);
                     console.log(value);
                     base.setState('colors', newColors);
-                    break;
-                        
-                default:
-                console.log(name);
+                    console.log(base.getState().colors);
+            }else if( name == 'nBoxes' ){
+                var diff = value - base.getState().nBoxes;
+                diff = Math.abs(diff);
 
+                if(value > base.getState().nBoxes){
+                    do{
+                        base.setState('colors', base.getState().colors.concat(['azul']));
+                        diff--;
+                    }while(diff > 0)
+                }else if(value < base.getState().nBoxes){
+                    var newColors = base.getState().colors;
+                        
+                    do{
+                        newColors.pop();
+                        base.setState('colors', newColors);
+                        diff--;
+                    }while(diff > 0)
+
+                }
+                base.setState(name, value);
+            }else if( name == 'wayHorizontal' || name == 'image' || name == 'rounded'){
+                base.setState(name, !!value);
+            }else{
+                console.log("no hizo nada");
             }
+
          
         },
         click: function(key){
