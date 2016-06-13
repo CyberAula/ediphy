5a4ca90534c884c61e1099fbab97587b08f75a72
Jorge Bellon <j.bellonc@gmail.com>
2016-06-13 20:32:47 +0200

    Fixed reorder in DaliBoxSortable

--- components/DaliBoxSortable.jsx
+++ components/DaliBoxSortable.jsx
@@ -97,10 +97,15 @@ export default class DaliBoxSortable extends Component {
         let list = jQuery(this.refs.sortableContainer);
         list.sortable({ handle: '.drag-handle' ,
             stop: (event, ui) => {
-                const reorderedIndexes = list.sortable('toArray', {attribute: 'data-reactid'});// Obtiene la nueva disposición de los elementos
-                const indexes = reorderedIndexes.map(el => el.split('$')[2]); //Coge solo la parte que indica el orden
-                list.sortable('cancel'); //Evita que se reordenen para que gestione la llamada Redux
-                this.props.onBoxReorder(indexes, this.props.id); // Cambia el estado pasando como parámetro el id del sortable y el nuevo orden de los elementos. Ahora el orden también se puede UNDO y REDO
+                let indexes = [];
+                let children = list[0].children;
+                for (let i = 0; i < children.length; i++){
+                    indexes.push(children[i].getAttribute("data-id"));
+                }
+                if(indexes.length !== 0) {
+                    this.props.onBoxReorder(indexes, this.props.id);
+                }
+                list.sortable('cancel');
         }});
     }
 
