77fdbf9e53606c101cb3e8fb886b41d2fd9ba441
Jorge Bellon <j.bellonc@gmail.com>
2016-07-12 18:44:40 +0200

    Pretending to use server for XML

--- components/XMLConfigModal.jsx
+++ components/XMLConfigModal.jsx
@@ -11,9 +11,9 @@ export default class XMLConfigModal extends Component {
 
                 <Modal.Body>
                     <Row  >
-                      <div ref={"container"}>
-                          Import XML
-                      </div>
+                        <div ref={"container"}>
+                            Import XML
+                        </div>
                     </Row>
                 </Modal.Body>
 
@@ -22,10 +22,24 @@ export default class XMLConfigModal extends Component {
                         this.props.onXMLEditorToggled();
                     }}>Cancel</Button>
                     <Button bsStyle="primary" onClick={e => {
-                        let state = this.props.toolbar.state;
-                        state["__xml"] = this.generateXMLFromView(state);
-                        Dali.Plugins.get(this.props.toolbar.config.name).forceUpdate(state, this.props.id);
-                        this.props.onXMLEditorToggled();
+                        let state = Object.assign({}, this.props.toolbar.state);
+                        let xml = this.generateXMLFromView(state);
+                        $.ajax({
+                            url: state["__xml_path"] ? state["__xml_path"] : Dali.Config.xml_path,
+                            type: state["__xml_path"] ? 'PUT' : 'POST',
+                            data: new XMLSerializer().serializeToString(xml),
+                            success: function(response, status, xhr) {
+
+                            },
+                            error: function(xhr, status, error) {
+
+                            },
+                            complete: function(xhr, status) {
+                                state["__xml_path"] = "ua2_ue10_ejer7.xml";
+                                Dali.Plugins.get(this.props.toolbar.config.name).forceUpdate(state, this.props.id);
+                                this.props.onXMLEditorToggled();
+                            }.bind(this)
+                        });
                     }}>Save changes</Button>
                 </Modal.Footer>
 
@@ -33,138 +47,138 @@ export default class XMLConfigModal extends Component {
         );
     }
 
-    componentDidUpdate(prevProps, prevState){
-        if(this.props.visible && !prevProps.visible){
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.visible && !prevProps.visible) {
             this.refs.container.innerHTML = this.generateViewFromXML(this.props.toolbar.state["__xml"])
         }
     }
 
-    componentDidMount(){
-        if(this.props.toolbar && this.props.toolbar.state["__xml"]){
+    componentDidMount() {
+        if (this.props.toolbar && this.props.toolbar.state["__xml"]) {
             this.refs.container.innerHtml = this.generateViewFromXML(this.props.toolbar.state["__xml"])
         }
     }
 
-    generateViewFromXML(xml){
+    generateViewFromXML(xml) {
 
         //Esto es por si se quiere hacer diccionarios de palabras
         var scormWords = ['tries', 'notaCorteGlobal', 'scoreBase', 'recordScore'];
 
-        var tipo = '', scorm = '', stuffItems = '', stuffItem = '', moreStuff = '', enunciado='', preguntas='', respuestas='', preguntasArray = [], respuestasArray = [], solArr = [], feedBack = '';
+        var tipo = '', scorm = '', stuffItems = '', stuffItem = '', moreStuff = '', enunciado = '', preguntas = '', respuestas = '', preguntasArray = [], respuestasArray = [], solArr = [], feedBack = '';
 
         const colX12 = '<div class="col-xs-12">', colX6 = '<div class="col-xs-6">', colX4 = '<div class="col-xs-4">', colX3 = '<div class="col-xs-3">', close = '</div>', raw = '<div class="raw">';
         var auxName = '', auxValue = '', auxTemplate = '';
 
         var x = xml.getElementsByTagName('ITEMS');
-        
-        for( i = 0; i < x.length; i++){
-          for( j = 0; j < x[i].attributes.length; j++){
-            auxName = x[i].attributes[j].name;
-            auxValue = x[i].attributes[j].value;
-            if(auxName.match('col') || auxName == 'esquinas' || auxName == 'grosLine' ){
-              stuffItems += colX3 + '<label for="'+auxName+'">'+auxName+':</label> <input class="form-control" type="text" autofocus id="'+auxName+'" value= "'+ auxValue +'">' + close;
-            }else if(scormWords.indexOf(auxName) >= 0){
-              scorm += colX3 + '<label for="'+auxName+'">'+auxName+':</label> <input class="form-control" type="text" autofocus id="'+auxName+'" value= "'+ auxValue +'">' + close;
-            }else if(auxName == 'obj'){
-              tipo += colX6 + '<label for="'+auxName+'">'+auxName+':</label> <input class="form-control"  type="text" autofocus id="'+auxName+'" value= "'+ auxValue +'">' + close;
-            }else{
-              moreStuff += colX3 + '<label for="'+auxName+'">'+auxName+':</label> <input class="form-control"  type="text" autofocus id="'+auxName+'" value= "'+ auxValue +'">' + close;
+
+        for (i = 0; i < x.length; i++) {
+            for (j = 0; j < x[i].attributes.length; j++) {
+                auxName = x[i].attributes[j].name;
+                auxValue = x[i].attributes[j].value;
+                if (auxName.match('col') || auxName == 'esquinas' || auxName == 'grosLine') {
+                    stuffItems += colX3 + '<label for="' + auxName + '">' + auxName + ':</label> <input class="form-control" type="text" autofocus id="' + auxName + '" value= "' + auxValue + '">' + close;
+                } else if (scormWords.indexOf(auxName) >= 0) {
+                    scorm += colX3 + '<label for="' + auxName + '">' + auxName + ':</label> <input class="form-control" type="text" autofocus id="' + auxName + '" value= "' + auxValue + '">' + close;
+                } else if (auxName == 'obj') {
+                    tipo += colX6 + '<label for="' + auxName + '">' + auxName + ':</label> <input class="form-control"  type="text" autofocus id="' + auxName + '" value= "' + auxValue + '">' + close;
+                } else {
+                    moreStuff += colX3 + '<label for="' + auxName + '">' + auxName + ':</label> <input class="form-control"  type="text" autofocus id="' + auxName + '" value= "' + auxValue + '">' + close;
+                }
             }
-          }
         }
 
         var y = xml.getElementsByTagName('ITEM');
-        
-        for( i = 0; i < y.length; i++){
 
-          for( var a = 0; a < y[i].attributes.length; a++){
-            auxName = y[i].attributes[a].name;
-            auxValue = y[i].attributes[a].value;
-            if(auxName == 'tipo'){
-              tipo += colX6 + '<label for="'+auxName+'">'+auxName+':</label> <input class="form-control" type="text" autofocus id="'+auxName+'" value= "'+ auxValue +'">' + close;
-            }else if(auxName == 'enunciado' || auxName == 'instrucciones' || auxName == 'img'){
-              enunciado += colX12 + '<label  for="'+auxName+'">'+auxName+':</label> <input class="form-control" type="text" autofocus id="'+auxName+'" value= "'+ auxValue +'">' + close;
-            }else if(auxName == 'anchoDrop' || auxName == 'anchoDrag' || auxName == 'sidesSpace'){
-              stuffItem += colX3 + '<label  for="'+auxName+'">'+auxName+':</label> <input class="form-control" type="text" autofocus id="'+auxName+'" value= "'+ auxValue +'">' + close;
-            }else if(auxName == 'sol'){
-              solArr = auxValue.split(',');
-              enunciado += colX12 + '<label  for="'+auxName+'">'+auxName+':</label> <input class="form-control"  type="text" autofocus id="'+auxName+'" value= "'+ auxValue +'">' + close;
-            }else{
-              moreStuff += colX3 + '<label for="'+auxName+'">'+auxName+':</label> <input class="form-control"  type="text" autofocus id="'+auxName+'" value= "'+ auxValue +'">' + close;
-            }
-          }
+        for (i = 0; i < y.length; i++) {
 
-          var numQuest = 0;
-          var numAns = 0;
-          
-          for(n = 0; n < y[i].childNodes.length; n++){
-            if(y[i].childNodes[n].tagName){
-              auxName = y[i].childNodes[n].tagName;
-              auxValue = y[i].childNodes[n].textContent;
-              if(auxName == 'DRAG'){
-                preguntasArray.push(auxValue);
-                preguntas += colX12 + '<label for="'+auxName+'">'+auxName+':</label> <input class="form-control" type="text" autofocus id="'+auxName+'-'+numQuest+'" value =" '+ auxValue +'">' + '<p>Solución:'+solArr.shift()+'</p>' + close;
-                numQuest++;
-              }else if(auxName == 'DROP'){
-                respuestasArray.push(auxValue);
-                respuestas += colX12 + '<label for="'+auxName+'">'+auxName+':</label> <input class="form-control"  type="text" autofocus id="'+auxName+'-'+numAns+'" value= "'+ auxValue +'">' + close;
-                numAns++;
-              }else if(auxName.match('FEED') || auxName == 'NOTRIED' || auxName == 'JUSTIFICACION' ){
-                feedBack += colX12 + '<label  for="'+auxName+'">'+auxName+':</label> <input class="form-control"  type="text" autofocus id="'+auxName+'" value= "'+ auxValue +'">' + close;
-              }else{
-                moreStuff += colX3 + '<label  for="'+auxName+'">'+auxName+':</label> <input class="form-control"  type="text" autofocus id="'+auxName+'" value="'+ auxValue +'">' + close;
-              }
+            for (var a = 0; a < y[i].attributes.length; a++) {
+                auxName = y[i].attributes[a].name;
+                auxValue = y[i].attributes[a].value;
+                if (auxName == 'tipo') {
+                    tipo += colX6 + '<label for="' + auxName + '">' + auxName + ':</label> <input class="form-control" type="text" autofocus id="' + auxName + '" value= "' + auxValue + '">' + close;
+                } else if (auxName == 'enunciado' || auxName == 'instrucciones' || auxName == 'img') {
+                    enunciado += colX12 + '<label  for="' + auxName + '">' + auxName + ':</label> <input class="form-control" type="text" autofocus id="' + auxName + '" value= "' + auxValue + '">' + close;
+                } else if (auxName == 'anchoDrop' || auxName == 'anchoDrag' || auxName == 'sidesSpace') {
+                    stuffItem += colX3 + '<label  for="' + auxName + '">' + auxName + ':</label> <input class="form-control" type="text" autofocus id="' + auxName + '" value= "' + auxValue + '">' + close;
+                } else if (auxName == 'sol') {
+                    solArr = auxValue.split(',');
+                    enunciado += colX12 + '<label  for="' + auxName + '">' + auxName + ':</label> <input class="form-control"  type="text" autofocus id="' + auxName + '" value= "' + auxValue + '">' + close;
+                } else {
+                    moreStuff += colX3 + '<label for="' + auxName + '">' + auxName + ':</label> <input class="form-control"  type="text" autofocus id="' + auxName + '" value= "' + auxValue + '">' + close;
+                }
             }
-          }
-        
+
+            var numQuest = 0;
+            var numAns = 0;
+
+            for (n = 0; n < y[i].childNodes.length; n++) {
+                if (y[i].childNodes[n].tagName) {
+                    auxName = y[i].childNodes[n].tagName;
+                    auxValue = y[i].childNodes[n].textContent;
+                    if (auxName == 'DRAG') {
+                        preguntasArray.push(auxValue);
+                        preguntas += colX12 + '<label for="' + auxName + '">' + auxName + ':</label> <input class="form-control" type="text" autofocus id="' + auxName + '-' + numQuest + '" value =" ' + auxValue + '">' + '<p>Solución:' + solArr.shift() + '</p>' + close;
+                        numQuest++;
+                    } else if (auxName == 'DROP') {
+                        respuestasArray.push(auxValue);
+                        respuestas += colX12 + '<label for="' + auxName + '">' + auxName + ':</label> <input class="form-control"  type="text" autofocus id="' + auxName + '-' + numAns + '" value= "' + auxValue + '">' + close;
+                        numAns++;
+                    } else if (auxName.match('FEED') || auxName == 'NOTRIED' || auxName == 'JUSTIFICACION') {
+                        feedBack += colX12 + '<label  for="' + auxName + '">' + auxName + ':</label> <input class="form-control"  type="text" autofocus id="' + auxName + '" value= "' + auxValue + '">' + close;
+                    } else {
+                        moreStuff += colX3 + '<label  for="' + auxName + '">' + auxName + ':</label> <input class="form-control"  type="text" autofocus id="' + auxName + '" value="' + auxValue + '">' + close;
+                    }
+                }
+            }
+
         }
 
-        preguntas = '<div id="preguntasRespuestas">'+preguntas;
+        preguntas = '<div id="preguntasRespuestas">' + preguntas;
         respuestas = respuestas + '</div>';
-        auxTemplate = '<div class="form-group">'+tipo + stuffItems + scorm + enunciado+ stuffItem + preguntas + respuestas +feedBack +moreStuff + '</div>';
+        auxTemplate = '<div class="form-group">' + tipo + stuffItems + scorm + enunciado + stuffItem + preguntas + respuestas + feedBack + moreStuff + '</div>';
 
         return auxTemplate;
 
     }
 
-    changeValue(namekey){
+    changeValue(namekey) {
         console.log(namekey);
     }
 
-    generateXMLFromView(state){
-      var auxXml = state['__xml'];
-      var inputs = this.refs.container.getElementsByTagName('input');
-      var x = auxXml.getElementsByTagName('ITEMS');
-      var y = auxXml.getElementsByTagName('ITEM');
-    
-      var auxVisitado = '';
-      var numVisitado =  0;
-      var nameValue = '';
-      var payloadIndex = 0;
+    generateXMLFromView(state) {
+        var auxXml = state['__xml'];
+        var inputs = this.refs.container.getElementsByTagName('input');
+        var x = auxXml.getElementsByTagName('ITEMS');
+        var y = auxXml.getElementsByTagName('ITEM');
 
-      for (i = 0; i < inputs.length; i++) {            
-        
-        nameValue = inputs[i].id;
+        var auxVisitado = '';
+        var numVisitado = 0;
+        var nameValue = '';
+        var payloadIndex = 0;
 
-        if(auxVisitado == nameValue){
-          numVisitado++;
-          nameValue = nameValue + '-'+numVisitado;
-        }else{
-          numVisitado = 0;
+        for (i = 0; i < inputs.length; i++) {
+
+            nameValue = inputs[i].id;
+
+            if (auxVisitado == nameValue) {
+                numVisitado++;
+                nameValue = nameValue + '-' + numVisitado;
+            } else {
+                numVisitado = 0;
+            }
+
+            if (x[0].attributes[nameValue]) {
+                auxXml.getElementsByTagName("ITEMS")[0].setAttribute(nameValue, inputs[i].value);
+            } else if (y[0].attributes[nameValue]) {
+                auxXml.getElementsByTagName("ITEM")[0].setAttribute(nameValue, inputs[i].value);
+            } else {
+                auxXml.getElementsByTagName("ITEM")[0].childNodes[payloadIndex].textContent = inputs[i].value;
+                payloadIndex++;
+            }
+
+            auxVisitado = inputs[i].id;
         }
 
-        if(x[0].attributes[nameValue]){
-          auxXml.getElementsByTagName("ITEMS")[0].setAttribute(nameValue,inputs[i].value);
-        }else if(y[0].attributes[nameValue]){
-          auxXml.getElementsByTagName("ITEM")[0].setAttribute(nameValue,inputs[i].value);
-        }else{
-          auxXml.getElementsByTagName("ITEM")[0].childNodes[payloadIndex].textContent = inputs[i].value;
-          payloadIndex++;
-        }
-
-        auxVisitado = inputs[i].id;
-      }
-
-      return auxXml;
+        return auxXml;
     }
 }
