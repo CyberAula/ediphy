4fcb7aaeea547450befddbccb5fdae98428e8de1
Berto_ABC <vermibeto@gmail.com>
2016-09-13 15:49:38 +0200

    Purge package.json and clean jslint erros

--- i18n.js
+++ i18n.js
@@ -3,7 +3,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import {es} from './locales/es.js';
 import {en} from './locales/en.js';
 
-var translations = {en: en, es: es}
+var translations = {en: en, es: es};
 
 i18n
   .use(LanguageDetector)
