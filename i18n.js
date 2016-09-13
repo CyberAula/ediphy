8d4e818809a944767d25c6066c9b081df06c729d
Berto_ABC <vermibeto@gmail.com>
2016-09-13 12:37:10 +0200

    Json added to i18nreact

--- i18n.js
+++ i18n.js
@@ -1,12 +1,14 @@
 
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import es from './locales/es.json';
+import en from './locales/en.json';
+
 
 i18n
   .use(LanguageDetector)
   .init({
     whitelist: ['es', 'en'],
-
     debug: true
   });
 
