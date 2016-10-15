2da1d576d0bd714ad1ba2aa82b6985c59e815d32
abenitoc <vermibeto@gmail.com>
2016-10-16 01:56:39 +0200

    Begin method to get index in dalititle

--- components/DaliTitle.jsx
+++ components/DaliTitle.jsx
@@ -2,31 +2,66 @@ import React, {Component} from 'react';
 import {Breadcrumb, BreadcrumbItem, Button} from 'react-bootstrap';
 
 export default class DaliTitle extends Component {
+    getActualIndex(size, level){
+        
+        let actual_parent = this.props.navItems[this.props.navItem.parent];
+
+        if (level === size){
+            let actual_index = (actual_parent.children.indexOf(this.props.navItem.id));
+            if (actual_index !== -1){
+                return (actual_index + 1) + ". ";
+            }
+        } else {
+            let actual_index;
+            let new_parent;
+            let actual_level;
+            let iteration = size - level;
+            for(let n = 0; n < iteration; n++){
+                actual_level = actual_parent;
+                actual_parent = actual_level.parent;
+            }
+            if(actual_parent !== undefined && actual_parent.children !== undefined){
+                return actual_parent.children.indexOf(actual_level.id);
+            } else {
+                return "";
+            }
+        }
+
+
+
+        //if(this.props.navItem.parent !== 0){
+        //    let actual_index = (this.props.navItems[this.props.navItem.parent].children.indexOf(this.props.navItem.id));
+        //    if (actual_index !== -1){
+        //        return (actual_index + 1) + ". ";
+        //    }
+        //}
+        //return "";
+    }
+
     render() {
         let content;
         let unidad = "";
         let currentstatus = this.props.isReduced;
         let hideButton = (this.props.titles.length <= 1 && !this.props.isReduced || this.props.titles.length === 0 && currentstatus);
+        let actualIndex = this.getActualIndex();
 
         if (currentstatus === 'reduced') {
             let titles = this.props.titles;
-            console.log(titles);
-            console.log(this.props.navItem);
-            console.log(this.props.navItems);
-
+            
             let actualTitle = titles[titles.length - 1];
             unidad = titles[0];
             content = React.createElement("div", {},
                 React.createElement("h3", {},
                     React.createElement(Breadcrumb, {style: {margin: 0, backgroundColor: 'inherit'}},
                         titles.map((item, index) => {
+                            console.log(titles);
                             if (index !== 0 && index !== titles.length - 1) {
-                                return React.createElement(BreadcrumbItem, {key: index}, item);
+                                return React.createElement(BreadcrumbItem, {key: index}, this.getActualIndex(titles.length, index) + item);
                             }
                         })
                     )
                 ),
-                React.createElement("h4", {style: {margin: 0}}, (this.props.navItem.level !== 1 ? this.props.navItems[this.props.navItem.parent].children.indexOf(this.props.id) : "") + actualTitle)
+                React.createElement("h4", {style: {margin: 0}}, actualIndex + actualTitle)
             );
 
         } else if (currentstatus === 'expanded') {
@@ -37,7 +72,7 @@ export default class DaliTitle extends Component {
                         unidad = text;
                     } else {
                         let nivel = (index > 4 ) ? 6 : index + 2;
-                        return React.createElement("h" + nivel, {key: index, style: {marginTop: '16px'}}, text);
+                        return React.createElement("h" + nivel, {key: index, style: {marginTop: '16px'}}, this.getActualIndex(this.props.titles.length, index) + text);
                     }
                 })
             );
