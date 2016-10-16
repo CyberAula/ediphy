3d896326988245116af2be15938bea6a1921350d
abenitoc <vermibeto@gmail.com>
2016-10-16 14:28:38 +0200

    Some leves missbehaving

--- components/DaliTitle.jsx
+++ components/DaliTitle.jsx
@@ -2,40 +2,49 @@ import React, {Component} from 'react';
 import {Breadcrumb, BreadcrumbItem, Button} from 'react-bootstrap';
 
 export default class DaliTitle extends Component {
+
     getActualIndex(size, level){
         
         let actual_parent = this.props.navItems[this.props.navItem.parent];
+        let actual_level = this.props.navItem;
+        size = size - 1;
 
-        if (level === size){
-            let actual_index = (actual_parent.children.indexOf(this.props.navItem.id));
+        if (size === undefined || level === undefined){
+            return "";
+        } else if(size === level){
+
+            let actual_index = (actual_parent.children.indexOf(actual_level.id));
             if (actual_index !== -1){
                 return (actual_index + 1) + ". ";
             }
+
         } else {
+
             let actual_index;
-            let new_parent;
-            let actual_level;
-            let iteration = size - level;
-            for(let n = 0; n < iteration; n++){
+            console.log("Level " + actual_level.level);
+
+            console.log("size" + size);
+            console.log("level" + level);
+
+            for(let n = level; n > 1; n--){
                 actual_level = actual_parent;
-                actual_parent = actual_level.parent;
+                actual_parent = this.props.navItems[actual_level.parent];
             }
+
+            console.log("final_parent");
+            console.log(actual_parent);
+            console.log("final_level");
+            console.log(actual_level);
+            console.log("final_index");
+            console.log(actual_parent.children.indexOf(actual_level.id) + 1);
+                
+            let final_level = actual_parent.children.indexOf(actual_level.id) + 1;
             if(actual_parent !== undefined && actual_parent.children !== undefined){
-                return actual_parent.children.indexOf(actual_level.id);
+                return final_level;
             } else {
                 return "";
             }
         }
-
-
-
-        //if(this.props.navItem.parent !== 0){
-        //    let actual_index = (this.props.navItems[this.props.navItem.parent].children.indexOf(this.props.navItem.id));
-        //    if (actual_index !== -1){
-        //        return (actual_index + 1) + ". ";
-        //    }
-        //}
-        //return "";
     }
 
     render() {
@@ -54,7 +63,7 @@ export default class DaliTitle extends Component {
                 React.createElement("h3", {},
                     React.createElement(Breadcrumb, {style: {margin: 0, backgroundColor: 'inherit'}},
                         titles.map((item, index) => {
-                            console.log(titles);
+                            //console.log(titles);
                             if (index !== 0 && index !== titles.length - 1) {
                                 return React.createElement(BreadcrumbItem, {key: index}, this.getActualIndex(titles.length, index) + item);
                             }
@@ -66,13 +75,14 @@ export default class DaliTitle extends Component {
 
         } else if (currentstatus === 'expanded') {
             let titlesComponents = "";
+            let titles_length = this.props.titles.length;
             content = React.createElement("div", {},
                 this.props.titles.map((text, index) => {
                     if (index === 0) {
                         unidad = text;
                     } else {
                         let nivel = (index > 4 ) ? 6 : index + 2;
-                        return React.createElement("h" + nivel, {key: index, style: {marginTop: '16px'}}, this.getActualIndex(this.props.titles.length, index) + text);
+                        return React.createElement("h" + nivel, {key: index, style: {marginTop: '16px'}}, this.getActualIndex(titles_length, index) + text);
                     }
                 })
             );
