c37f4645dd4c3793114144172c76e57c2d55ba3a
Berto_ABC <vermibeto@gmail.com>
2016-11-17 20:34:13 +0100

    HOT NEWS ON THE AIR ZIP CREATION IS HERE

--- webpack_plugins/bundle_zip_plugin.js
+++ webpack_plugins/bundle_zip_plugin.js
@@ -1,53 +1,189 @@
 var fs = require("fs");
 var JSZip = require("jszip");
+var async = require("async");
+var dir = require('node-dir');
+
+var visor_zip = new JSZip();
+var scorm_zip = new JSZip();
 
 function ZipBundlePlugin(){
   this.startTime = Date.now();
 }
 
+function purgeRoot(str){
+  return str.replace("dist\/","");
+}
+
+
 ZipBundlePlugin.prototype.apply = function(compiler){
+
+  /* Visor ZIP generation */
   compiler.plugin("after-emit",function(compilation, callback){
 
-    fs.readFile("dist/lib/visor/dist.zip", function(err, data) {
-        if (err) throw err;
-        var jszip = new JSZip();
-        jszip.loadAsync(data).then(function (zip) {
-            fs.readFile("dist/js/visor-bundle.js", function(err, data) {
-              zip.remove("js/visor-bundle.js");
-            zip.file("js/visor-bundle.js", data);
-            zip.generateNodeStream({type:'nodebuffer',streamFiles:true})
-          .pipe(fs.createWriteStream("dist/lib/visor/dist.zip"))
-          .on('finish', function () {
-              // JSZip generates a readable stream with a "end" event,
-              // but is piped here in a writable stream which emits a "finish" event.
-              console.log("TASK: dist.zip updated.");
-              callback();
+    async.series([
+        function(callback){
+          var path = "./dist/css/";
+          fs.stat(path, function(err, stats) {
+            if(err){
+              console.log("/dist/css/ does not exist!");
+              callback(null, "css");
+            }else{
+              dir.files(path, function(err, filelist) {
+                  if (err) throw err;
+                  async.each(filelist, function(elem,call){
+                    visor_zip.file(purgeRoot(elem), fs.readFileSync("./" + elem));
+                    scorm_zip.file(purgeRoot(elem), fs.readFileSync("./" + elem));
+                    call();
+                  }, function(err,results){
+                    callback(null, "css");
+                  });
+              });
+            }
           });
-        });
-        });
-    });
+        },
+        function(callback){
+          var path = "./dist/images/";
+          fs.stat(path, function(err, stats) {
+            if(err){
+              console.log("/dist/images/ does not exist!");
+              callback(null, "images");
+            }else{
+              dir.files(path, function(err, filelist) {
+                  if (err) throw err;
+                  async.each(filelist, function(elem,call){
+                    visor_zip.file(purgeRoot(elem), fs.readFileSync("./" +elem));
+                    scorm_zip.file(purgeRoot(elem), fs.readFileSync("./" + elem));
+                    call();
+                  }, function(err,results){
+                    callback(null, "images");
+                  });
+              });
+            }
+          });
+        },
+        function(callback){
+          var path = "./dist/src/";
+          fs.stat(path, function(err, stats) {
+            if(err){
+              console.log("/dist/src/ does not exist!");
+              callback(null, "src");
+            }else{
+              dir.files(path, function(err, filelist) {
+                  if (err) throw err;
+                  async.each(filelist, function(elem,call){
+                    visor_zip.file(purgeRoot(elem), fs.readFileSync("./" +elem));
+                    scorm_zip.file(purgeRoot(elem), fs.readFileSync("./" + elem));
+                    call();
+                  }, function(err,results){
+                    callback(null, "src");
+                  });
+              });
+            }
+          });
+        },
+        function(callback){
+            var path = "./dist/js/";
+            fs.stat(path, function(err, stats) {
+            if(err){
+              console.log("/dist/js/ does not exist!");
+              callback(null, "js");
+            }else{
+              dir.files(path, function(err, filelist) {
+                  if (err) throw err;
+                  async.each(filelist, function(elem,call){
+                    visor_zip.file(purgeRoot(elem), fs.readFileSync("./" +elem));
+                    scorm_zip.file(purgeRoot(elem), fs.readFileSync("./" + elem));
+                    call();
+                  }, function(err,results){
+                    callback(null, "js");
+                  });
+              });
+            }
+          });
+        },
+        function(callback){
+          var path = "./dist/exercises/";
+          fs.stat(path, function(err, stats) {
+            if(err){
+              console.log("/dist/exercises/ does not exist!");
+              callback(null, "exercises");
+            }else{
+              dir.files(path, function(err, filelist) {
+                  if (err) throw err;
+                  async.each(filelist, function(elem,call){
+                    visor_zip.file(purgeRoot(elem), fs.readFileSync("./" +elem));
+                    scorm_zip.file(purgeRoot(elem), fs.readFileSync("./" + elem));
+                    call();
+                  }, function(err,results){
+                    callback(null, "exercises");
+                  });
+              });
+            }
+          });
+        },
+        function(callback){
+          var path = "./dist/lib/scorm/scorm/";
+          fs.stat(path, function(err, stats) {
+            if(err){
+              console.log("./dist/lib/scorm/scorm/ does not exist!");
+              callback(null, "scorm");
+            }else{
+              dir.files(path, function(err, filelist) {
+                  if (err) throw err;
+                  async.each(filelist, function(elem,call){
+                    scorm_zip.file(elem.replace("dist\/lib\/scorm\/",""), fs.readFileSync("./" + elem));
+                    call();
+                  }, function(err,results){
+                    callback(null, "scorm");
+                  });
+              });
+            }
+          });
+        },
+        function(callback){
+          var path = "./dist/lib/scorm/manifest_files/";
+          fs.stat(path, function(err, stats) {
+            if(err){
+              console.log("/dist/exercises/ does not exist!");
+              callback(null, "manifest_files");
+            }else{
+              dir.files(path, function(err, filelist) {
+                  if (err) throw err;
+                  async.each(filelist, function(elem,call){
+                    scorm_zip.file(elem.replace("dist\/lib\/scorm\/manifest_files/",""), fs.readFileSync("./" + elem));
+                    call();
+                  }, function(err,results){
+                    callback(null, "manifest_files");
+                  });
+              });
+            }
+          });
+        },        
+        function(callback){
+            var path = "./dist/visor-bundle.js";
+            fs.stat(path, function(err, stats) {
+            if(err){
+              console.log("js/visor-bundle.js does not exist!");
+              callback(null, "bundle");
+            }else{
+              visor_zip.file("js/visor-bundle.js",fs.readFile(path));
+              scorm_zip.file("js/visor-bundle.js",fs.readFile(path));
+              callback(null, "bundle");
+            } 
+          });
+        }
+        ],function(err,results){
+           visor_zip.generateAsync({type:"nodebuffer"}).then(function (content) {
+              fs.writeFile("./dist/lib/visor/dist.zip", content, function(err){});
+              console.log("Ended dist.zip bundle");
+          });
+           scorm_zip.generateAsync({type:"nodebuffer"}).then(function (content) {
+              fs.writeFile("./dist/lib/scorm/scorm.zip", content, function(err){});
+              console.log("Ended scorm.zip bundle");
+          });
+        }
+      );
 
-  });
-
-  compiler.plugin("after-emit",function(compilation, callback){
-    fs.readFile("dist/lib/scorm/scorm.zip", function(err, data) {
-      if (err) throw err;
-      var jszip = new JSZip();
-      jszip.loadAsync(data).then(function (zip) {
-          fs.readFile("dist/js/visor-bundle.js", function(err, data) {
-            zip.remove("js/visor-bundle.js");
-          zip.file("js/visor-bundle.js", data);
-          zip.generateNodeStream({type:'nodebuffer',streamFiles:true})
-        .pipe(fs.createWriteStream("dist/lib/scorm/scorm.zip"))
-        .on('finish', function () {
-            // JSZip generates a readable stream with a "end" event,
-            // but is piped here in a writable stream which emits a "finish" event.
-            console.log("TASK: scorm.zip updated.");
-            callback();
-        });
-      });
-      });
-    });
   });
 }
 
