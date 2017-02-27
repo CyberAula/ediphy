31bbd2aabe5d38a057e60d133c4c229cbfed7da3
abenitoc <bertocode@gmail.com>
2017-02-27 17:08:53 +0100

    Initial commit

---
+++ components/navigator/Navigator.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Modal, FormControl, Col, Form, FormGroup, ControlLabel, Button} from 'react-bootstrap';
+import Dali from './../../../core/main';
+
+export default class Navigator extends Component {
+
+    render(){
+
+    }
+
+}
