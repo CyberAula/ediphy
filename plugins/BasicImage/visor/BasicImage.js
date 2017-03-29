98b5251318ce01885dd2703d958c11d76338cfc7
abenitoc <vermibeto@gmail.com>
2017-03-29 15:17:30 +0200

    idk

--- plugins/BasicImage/visor/BasicImage.js
+++ plugins/BasicImage/visor/BasicImage.js
@@ -4,13 +4,10 @@ export function BasicImage(base) {
             base.registerExtraFunction(this.imageClick, "click");
         },
         getRenderTemplate: function (state) {
-            return "<div style=\"width: 100%; height: 100%\">" +
-                "<img  class=\"basicImageClass\" " + "style=\"width: 100%; " +
-                "height: 100%; left:0px; top:0px;\"" +
-                "src=\"" + state.url + "\" onclick='$dali$.imageClick()'/>" +
-                "</div>";
+            return "<div style=\"width: 100%; height: 100%\"> <img class=\"basicImageClass\" " + "style=\"width: 100%; height: 100%; left:0px; top:0px;\" src='" + state.url + "' onclick='$dali$.imageClick()' /></div>";
         },
         imageClick: function (e) {
+            alert('viva españa');
         }
     };
 }
