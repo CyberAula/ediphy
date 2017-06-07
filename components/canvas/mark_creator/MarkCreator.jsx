4cb26c7028972842c180180a53b69f3e82b3f2ed
abenitoc <vermibeto@gmail.com>
2017-06-07 16:53:05 +0200

    Added cursor change

--- components/canvas/mark_creator/MarkCreator.jsx
+++ components/canvas/mark_creator/MarkCreator.jsx
@@ -8,4 +8,11 @@ export default class MarkCreator extends Component {
         /* jshint ignore:end */
     }
 
+    componentWillUpdate(nextProps){
+        if(nextProps.MarkCreatorId !== false){
+            //change to hover
+            document.documentElement.style.cursor = 'url("https://storage.googleapis.com/material-icons/external-assets/v4/icons/svg/ic_room_black_24px.svg"), default';
+        }
+    }
+
 }
