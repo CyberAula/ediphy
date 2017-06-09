303059e9723aa8d997fae93a40deeeb7bb8cc70c
abenitoc <bertocode@gmail.com>
2017-06-09 15:20:39 +0200

    Added window object as workaround

--- components/canvas/dali_canvas_sli/DaliCanvasSli.jsx
+++ components/canvas/dali_canvas_sli/DaliCanvasSli.jsx
@@ -181,9 +181,11 @@ export default class DaliCanvasSli extends Component {
         canvas.style.height="100%";
         canvas.style.width="100%";
         let ratio;
+        /* this is to avoid get values from react flow when using event listeners that do not exist in react
+         * get the values from window.object */
         if(window.canvasRatio === undefined){
             ratio = this.props.canvasRatio;
-            window.canvasRatio = this.props.canvasRatio;
+            window.canvasRatio = this.props.canvasRatio; //https://stackoverflow.com/questions/19014250/reactjs-rerender-on-browser-resize
         } else {
             ratio = window.canvasRatio;
         }
