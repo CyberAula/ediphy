affee2254e0bec1cf44a5ce06a3eb7adc032e109
Lourdesmarco <lourdesmarco.design@gmail.com>
2017-07-13 15:48:09 +0200

    enrich video styles

--- plugins/EnrichedPlayer/locales/es.js
+++ plugins/EnrichedPlayer/locales/es.js
@@ -12,7 +12,7 @@ module.exports = {
         "box_style": "Estilo caja",
         "opacity": "Opacidad",
         "padding": "Padding",
-        "PluginName": "Reproductor con puntos de interés",
+        "PluginName": "Reproductor enriquecido",
         "radius": "Radio",
         "source": "Origen"
     }
