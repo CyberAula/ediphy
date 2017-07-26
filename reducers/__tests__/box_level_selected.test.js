c13649ac119a810fc33472949fb3c4a29479efe4
Unknown <carlos.cuencae@alumnos.upm.es>
2017-07-26 13:26:08 +0200

    Added more test and testHelper

--- reducers/__tests__/box_level_selected.test.js
+++ reducers/__tests__/box_level_selected.test.js
@@ -1,46 +1,60 @@
+import helper from './test_helper';
 import reducer from '../box_level_selected';
 import {ADD_BOX, INCREASE_LEVEL, SELECT_BOX, SELECT_NAV_ITEM, DELETE_NAV_ITEM} from '../../actions';
 import {ID_PREFIX_SORTABLE_BOX} from '../../constants';
 
+let reducerHelper = helper(undefined, reducer);
+
 describe('box_level_selected reducer', () => {
-  it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual(0)
-  });
 
-  it('should handle ADD_BOX', () => {
-    expect(reducer(undefined, {type: ADD_BOX})).toEqual(0);
-    for (var i = 0; i < 10; i++) {
-      expect(reducer(i, {type: ADD_BOX})).toEqual(0);
-    }
-  });
+	beforeAll(() => {
+		reducerHelper.clean();
+	});
 
-  it('should handle INCREASE_LEVEL', () => {
-    for (var i = 0; i < 10; i++) {
-      reducer(i, {type: INCREASE_LEVEL});
-    }
-    expect(reducer(i, {type: INCREASE_LEVEL})).toEqual(i + 1);
-  });
+	test('should return the initial state', () => {
+		expect(reducer(undefined, {})).toEqual(0)
+	});
 
-  it('should handle DELETE_NAV_ITEM', () => {
-    for (var i = 0; i < 10; i++) {
-      expect(reducer(i, {type: DELETE_NAV_ITEM})).toEqual(0);
-    }
-  });
+	test('should handle ADD_BOX', () => {
+		reducerHelper.call({type: ADD_BOX});
+		expect(reducerHelper.state).toEqual(0);
+	});
 
-  it('should handle SELECT_BOX', () => {
-    let state = 0;
-    expect(reducer(undefined, {type: SELECT_BOX, payload:{id:-1}})).toEqual(0);
-    expect(reducer(undefined, {type: SELECT_BOX, payload:{id:"85584"}})).toEqual(0);
-    expect(reducer(undefined, {type: SELECT_BOX, payload:{id:"bg6565"}})).toEqual(0);
-    expect(reducer(undefined, {type: SELECT_BOX, payload:{id:ID_PREFIX_SORTABLE_BOX + "2"}})).toEqual(-1);
+	test('should handle INCREASE_LEVEL', () => {
+		for (var i = 0; i < 10; i++) {
+			reducerHelper.call({type: INCREASE_LEVEL});
+		}
+		expect(reducerHelper.state).toEqual(i);
+	});
 
-    reducer(state++, {type: INCREASE_LEVEL});
-    reducer(state++, {type: INCREASE_LEVEL});
-    expect(reducer(state, {type: SELECT_BOX, payload:{id:"85584"}})).toEqual(state);
+	test('should handle DELETE_NAV_ITEM', () => {
+		for (var i = 0; i < 10; i++) {
+			reducerHelper.call({type: DELETE_NAV_ITEM});
+		}
+		expect(reducerHelper.state).toEqual(0);
+	});
 
-  });
+	test('should handle SELECT_BOX', () => {
+		reducerHelper.call({type: SELECT_BOX, payload:{id:-1}});
+		expect(reducerHelper.state).toEqual(0);
+		reducerHelper.call({type: SELECT_BOX, payload:{id:"34534534543"}});
+		expect(reducerHelper.state).toEqual(0);
+		reducerHelper.call({type: SELECT_BOX, payload:{id:"bg-23424234"}});
+		expect(reducerHelper.state).toEqual(0);
+		reducerHelper.call({type: SELECT_BOX, payload:{id:ID_PREFIX_SORTABLE_BOX + "2"}});
+		expect(reducerHelper.state).toEqual(-1);
 
-  it('should handle SELECT_NAV_ITEM', () => {
-    expect(reducer(undefined, {type: SELECT_NAV_ITEM})).toEqual(0);
-  });
+		reducerHelper.call({type: SELECT_BOX, payload:{id:"85584"}});
+		expect(reducerHelper.state).toEqual(0);
+		reducerHelper.call({type: INCREASE_LEVEL});
+		reducerHelper.call({type: INCREASE_LEVEL});
+		reducerHelper.call({type: SELECT_BOX, payload:{id:"asd-85584"}});
+		expect(reducerHelper.state).toEqual(2);
+
+	});
+
+	test('should handle SELECT_NAV_ITEM', () => {
+		reducerHelper.call({type: SELECT_NAV_ITEM});
+		expect(reducerHelper.state).toEqual(0);
+	});
 })
