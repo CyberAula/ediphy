82f27d7d8964c8bfa985204301fc16e3492d1b19
abenitoc <bertocode@gmail.com>
2017-08-28 16:50:21 +0200

    Upgrade webpack 1 to 2 and some cleaning

--- webpack.config.js
+++ webpack.config.js
@@ -5,137 +5,90 @@ let dependency_loader = require('./webpack_plugins/dependencies_loader.js');
 module.exports = {
     devtool: 'source-map',
     entry: {
-        'app': [
+        'app':[
             'webpack-dev-server/client?http://localhost:8080', // WebpackDevServer host and port
             'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
-            'bootstrap-loader', // Loads Twitter Bootstrap
-            './index.jsx', // Appʼs entry point
-        ],
-        'js/visor': './_visor/containers/EditorVisor.jsx',
+            'bootstrap-loader', //Loads Twitter Bootstrap
+            __dirname + '/index.jsx'], // Appʼs entry point
+        'js/visor': __dirname + '/_visor/containers/EditorVisor.jsx',
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/', // This is used to generate URLs to e.g. images
+        filename: '[name]-bundle.js',
     },
     module: {
-        /* preLoaders: [
+        rules: [
             {
+                enforce: 'pre',
                 test: /\.(es6|jsx|js)$/,
+                use: ['eslint-loader'],
                 exclude: [/node_modules/],
-                loader: 'jshint-loader'
-            }
-        ],*/
-        preLoaders: [
-            {
-                test: /\.(es6|jsx|js)$/,
-                exclude: [/node_modules/],
-                loader: 'eslint-loader',
             },
-        ],
-        loaders: [
             {
                 test: /\.es6$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015'],
-                    plugins: ['transform-object-rest-spread'],
-                },
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015'],
+                        plugins: [require('babel-plugin-transform-object-rest-spread')]
+                    }
+                }
             },
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'react-hot-loader/webpack!babel-loader?presets[]=es2015,presets[]=react',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015','react'],
+                    }
+                }
             },
             {
                 test: /\.css$/,
-                // exclude: /node_modules/,
-                loader: 'style-loader!css-loader',
+                use: [
+                    "style-loader",
+                    "css-loader",
+                ]
             },
             {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                loader: 'style-loader!css-loader!sass-loader',
+                test: /\.(scss|sass)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    { loader: 'sass-loader', options: { sourceMap: true } }
+                ],
             },
             {
-                test: /\.(jpg|gif|png)$/,
-                loader: 'url-loader?limit=100000',
-            },
-            /* {
-                test: /\.(woff2?|svg)$/,
-                loader: 'url-loader?limit=10000' },*/
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff",
-            },
-
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file-loader" },
-            /* {
-                test: /\.(ttf|eot)$/,
-                loader: 'file-loader',
-            }*/
-            {
-                test: /\.json$/,
-                loader: 'json-loader',
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2|json|xml|ico)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 100000
+                        }
+                    }
+                ]
             },
             {
                 test: require.resolve('jquery'),
-                loader: 'expose?jQuery!expose?$!expose?window.jQuery', // expose-loader, exposes as global variable
+                use: [{
+                    loader: 'expose-loader',
+                    options: '$'
+                }]
             },
         ].concat(dependency_loader.getExposeString()),
     },
     resolve: {
-        extensions: ['', '.js', '.jsx', '.es6'],
-    },
-    output: {
-        path: './dist',
-        publicPath: '/', // This is used to generate URLs to e.g. images
-        filename: '[name]-bundle.js',
+        //resolve.alias could be useful for resolving certain modules easily
+        extensions: ['.js', '.jsx', '.es6'],
     },
     devServer: {
         contentBase: './dist',
         hot: true,
-    },
-
-    eslint: { configFile: './.eslintrc' },
-    jshint: {
-        // http://www.jshint.com/docs/options/
-
-        // This option prohibits the use of bitwise operators such as ^ (XOR), | (OR) and others.
-        // Bitwise operators are very rare in JavaScript programs and quite often & is simply a mistyped &&
-        bitwise: true,
-
-        // This option requires you to always put curly braces around blocks in loops and conditionals
-        curly: true,
-
-        // This options prohibits the use of == and != in favor of === and !==
-        eqeqeq: true,
-
-        // This option is used to specify the ECMAScript version to which the code must adhere
-        esversion: 6,
-
-        // This option requires all for in loops to filter object's items. The for in statement
-        // allows for looping through the names of all of the properties of an object including
-        // those inherited through the prototype chain.
-        // forin: true,
-
-        // This options prohibits overwriting prototypes of native objects such as Array, Date and so on.
-        freeze: true,
-
-        // This option requires the code to run in ECMAScript 5's strict mode.
-        // strict: true,
-
-        // This option prohibits the use of explicitly undeclared variables.
-        undef: true,
-
-        // This option warns when you define and never use your variables.
-        unused: false,
-
-        // This option suppresses warnings about functions inside of loops.
-        loopfunc: true,
-
-        // These options let JSHint know about some pre-defined global variables
-        browser: true,
-        devel: true,
-        jquery: true,
-        predef: ["Dali", "html2json", "CKEDITOR", "EJS"].concat(dependency_loader.getJSHintExludeNames()),
-
     },
     plugins: [
         new webpack.ContextReplacementPlugin(/package\.json$/, "./plugins/"),
