632c8c9509c4c7632d3f762945f508d82064a900
abenitoc <vermibeto@gmail.com>
2017-09-08 14:44:25 +0200

    Fix lint problems update package.json, removed imageclick event

--- plugins/BasicImage/visor/BasicImage.js
+++ plugins/BasicImage/visor/BasicImage.js
@@ -6,19 +6,17 @@ export function BasicImage(base) {
             base.registerExtraFunction(this.imageClick, "click");
         },
         getRenderTemplate: function(state) {
-
             let ImageComponent = () => {
                 return <div style={{ width: '100%', height: '100%' }}>
                     <img style={{ width: '100%', height: '100%', left: '0px', top: '0px' }}
                         src={state.url}
-                        onClick={()=>{ this.imageClick();}} />
+                        /* onClick={()=>{ this.imageClick();}} */ />
                 </div>;
             };
             return <ImageComponent/>;
-
-        },
+        }, /*
         imageClick: function(e) {
             alert('Im a magnificent log alert');
-        },
+        },*/
     };
 }
