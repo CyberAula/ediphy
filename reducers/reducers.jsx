45faa74d6fbab98e013fe2fe13bfa4a450fada8b
sonsoleslp <sonsoleslp@hotmail.com>
2017-09-22 18:38:01 +0200

    Remove UPDATE_BOX from UNDO filters

--- reducers/reducers.jsx
+++ reducers/reducers.jsx
@@ -86,9 +86,8 @@ const GlobalState = undoable(combineReducers({
         case TOGGLE_TEXT_EDITOR:
         case TOGGLE_TITLE_MODE:
         case UPDATE_NAV_ITEM_EXTRA_FILES:
-        case UPDATE_BOX:
-        /* case DELETE_RICH_MARK:
-         case ADD_RICH_MARK:*/
+        // case UPDATE_BOX:
+        //  case ADD_RICH_MARK:
             return false;
         }
 
