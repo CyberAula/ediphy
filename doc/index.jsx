248fde61b3bae1d95770e40d4f90ad6604b54960
sonsoleslp <sonsoleslp@hotmail.com>
2017-09-24 17:41:09 +0200

    Add hash router in docs

--- doc/index.jsx
+++ doc/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import createBrowserHistory from 'history/createBrowserHistory';
 const supportsHistory = 'pushState' in window.history;
