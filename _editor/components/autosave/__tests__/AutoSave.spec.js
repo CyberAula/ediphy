8d43abea38fd24cab2f8d03ce9effbfedbc57970
Lourdesmarco <lourdesmarco.design@gmail.com>
2017-11-20 14:18:35 +0100

    delete coverage trash & minor changes

--- _editor/components/autosave/__tests__/AutoSave.spec.js
+++ _editor/components/autosave/__tests__/AutoSave.spec.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import expect from 'expect';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
+import { shallowToJson } from 'enzyme-to-json';
 import AutoSave from "../AutoSave.jsx";
 
 describe('Autosave', () => {
-    it('AutoSave renders Saving...', () => {
-        const saving = shallow(<AutoSave save={() => {this.dispatchAndSetState(exportStateAsync({ present: this.props.store.getState().present }));}}/>);
-        expect(toJson(saving)).toMatchSnapshot();
+    it('should render with isRequired props', () => {
+        const saving = shallow(
+            <AutoSave save={() => { console.log("Saving..."); }}/>);
+        expect(shallowToJson(saving)).toMatchSnapshot();
     });
 });
