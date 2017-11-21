ad14aef84dd5caa5ac844d94ab96c9002c4331e7
abenitoc <bertocode@gmail.com>
2017-11-21 12:30:53 +0100

    Using yarn, letting submodules to be installed by yarn- node 6 && yarn 1.3

--- plugins/__tests__/plugin_tester.test.js
+++ plugins/__tests__/plugin_tester.test.js
@@ -1,30 +1,14 @@
 import glob from 'glob';
 
-const plugins = getPluginProvider();
+const plugins = getPluginJSONs();
 
-function getPluginProvider() {
-    let files = glob.sync("plugins/*/package.json");
-    console.log(files);
-    let final_object = {};
-    for(let packageit in files) {
-        let json = stub.returns(require.requireActual("../" + files[packageit]));
-        let dependencies = json.dependencies;
-        var config = json.config;
-        if(dependencies) {
-            Object.keys(dependencies).map(function(e) {
-                if(config && config.aliases && config.aliases[e]) {
-                    final_object[config.aliases[e]] = e;
-                } else {
-                    final_object[toCamelCase(e)] = e;
-                }
-            });
-        }
-    }
-    return final_object;
+function getPluginJSONs() {
+    return glob.sync("plugins/*/package.json");
 }
 
 describe('test is well formed', ()=>{
     test('does_get_plugin', ()=>{
+        console.log(plugins);
         expect(plugins).toBeDefined();
     });
 });
