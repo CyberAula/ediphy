c5a9357b94fc319101e071ed785732dbba397bf1
Lourdesmarco <lourdesmarco.design@gmail.com>
2017-11-22 07:48:49 +0100

    more testing

--- reducers/__tests__/global_config.test.js
+++ reducers/__tests__/global_config.test.js
@@ -3,23 +3,11 @@ import * as types from '../../common/actions';
 
 describe('global_config reducer', ()=>{
     it('should return 0 as default initial state', () => {
-        // setup
-        let action = { type: 'unknown' };
-        // execute
-        let newState = global_config(0, action);
 
-        expect(newState).toEqual(0);
     });
     describe('handle IMPORT_STATE', () => {
         it('should load globalConfig ', () => {
-            // setup
-            let action = {
-                type: types.IMPORT_STATE,
-            };
-            // execute
-            let newState = index_selected(undefined, action);
 
-            // expect(newState).toEqual();
         });
     });
 
--- reducers/__tests__/index_selected.test.js
+++ reducers/__tests__/index_selected.test.js
@@ -1,50 +1,30 @@
 import { testState } from '../../core/store/state.tests.js';
-import index_selectedReducer from '../index_selected';
-import * as types from '../../common/actions';
-import * as prefixes from '../../common/constants';
+import index_selected from '../index_selected';
+import * as ActionTypes from '../../common/actions';
 
-let initstate = testState;
-// console.log(initstate);
+const initstate = testState;
 
-let random = Math.floor(Math.random() * (100000 - 10000 + 1)) + 10000;
+describe('# index_selected reducer', ()=>{
 
-describe('index_selected reducer', ()=>{
-
-    it('should return testState as default initial state', () => {
-
-        // execute
-        let newState = index_selectedReducer(initstate, {});
-
-        expect(newState).toEqual(initstate);
+    test('should return testState as initial state', () => {
+        expect(index_selected(initstate, {})).toEqual(initstate);
     });
 
-    describe('should delete nav item and return 0', () => {
-        it('handle DELETE_NAV_ITEM', () => {
-            // setup
-            let action = { type: types.DELETE_NAV_ITEM }; // TODO: change for deleteNavItem(ids, parent, boxes, containedViews, linkedBoxes)
-
-            // execute
-            let newState = index_selectedReducer(initstate, action);
-
-            expect(newState).toEqual(0);
-
-        });
+    test('Delete navigation item', () => {
+        const action = { type: ActionTypes.DELETE_NAV_ITEM };
+        expect(index_selected(initstate, action)).toEqual(0);
     });
 
-    describe('should add nav item (handle ADD_NAV_ITEM)', () => {
-        it('should add CONTAINED_VIEW ', () => {
-            let cv = 'cv-1511252975056';
-            // setup
-            let action = {
-                type: types.ADD_NAV_ITEM,
-                payload: { id: cv },
-            };
-            // execute
-            let newState = index_selectedReducer(initstate, action);
-            // console.log(newState);
+    test('Add navigation item', () => {
+        const action = { type: ActionTypes.ADD_NAV_ITEM, payload: { id: 'pa-1511252985429' } };
+        expect(index_selected(initstate, action)).toEqual('pa-1511252985429');
 
-            expect(newState).toEqual(action.payload.id);
-        });
+    });
+
+    test('Select navigation item', () => {
+        const action = { type: ActionTypes.SELECT_NAV_ITEM, payload: { id: 'pa-1497983247795' } };
+        expect(index_selected(initstate, action)).toEqual(initstate);
+
     });
 
     describe('should select nav item ', () => {
@@ -52,12 +32,12 @@ describe('index_selected reducer', ()=>{
         it('handle SELECT_NAV_ITEM', () => {
             // setup
             let action = {
-                type: types.SELECT_NAV_ITEM,
+                type: ActionTypes.SELECT_NAV_ITEM,
                 payload: { id: cv },
             };
-                // types.selectNavItem('cv-1511252975055') ;
+                // ActionTypes.selectNavItem('cv-1511252975055') ;
             // execute
-            let newState = index_selectedReducer(cv, action);
+            let newState = index_selected(cv, action);
             // console.log(newState);
 
             expect(newState).toEqual(action.payload.id); // return state, now is 0
@@ -69,38 +49,38 @@ describe('index_selected reducer', ()=>{
 
     describe('should select an item (handle INDEX_SELECT)', () => {
         it('should select a PAGE', () => {
-            let random_page = prefixes.ID_PREFIX_PAGE + random;
+            let random_page = 'pa-1497983247795';
             // setup
             let action = {
-                type: types.INDEX_SELECT,
+                type: ActionTypes.INDEX_SELECT,
                 payload: { id: random_page },
             };
             // execute
-            let newState = index_selectedReducer(undefined, action);
+            let newState = index_selected(undefined, action);
 
             expect(newState).toEqual(random_page);
         });
         it('should select a SECTION', () => {
-            let random_section = prefixes.ID_PREFIX_SECTION + random;
+            let random_section = 'se-1467887497411';
             // setup
             let action = {
-                type: types.INDEX_SELECT,
+                type: ActionTypes.INDEX_SELECT,
                 payload: { id: random_section },
             };
             // execute
-            let newState = index_selectedReducer(undefined, action);
+            let newState = index_selected(undefined, action);
 
             expect(newState).toEqual(random_section);
         });
         it('should select a SLIDE', () => {
-            let random_slide = prefixes.ID_PREFIX_SLIDE + random;
+            let random_slide = 'pa-1511252955865';
             // setup
             let action = {
-                type: types.INDEX_SELECT,
+                type: ActionTypes.INDEX_SELECT,
                 payload: { id: random_slide },
             };
             // execute
-            let newState = index_selectedReducer(undefined, action);
+            let newState = index_selected(undefined, action);
 
             expect(newState).toEqual(random_slide);
         });
@@ -110,9 +90,9 @@ describe('index_selected reducer', ()=>{
     describe('should import state and return 0 ', () => {
         it('handle IMPORT_STATE', () => {
             // setup
-            let action = { type: types.IMPORT_STATE };
+            let action = { type: ActionTypes.IMPORT_STATE };
             // execute
-            let newState = index_selectedReducer(initstate, action);
+            let newState = index_selected(initstate, action);
 
             expect(newState).toEqual(0);
         });
