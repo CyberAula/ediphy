480a1da7cda4c994896f0a241ab52262ffa9d47a
sonsoleslp <sonsoleslp@hotmail.com>
2017-12-19 23:01:37 +0100

    Plugin creation CLI

--- plugins/create_plugin.js
+++ plugins/create_plugin.js
@@ -1,11 +1,16 @@
+/**
+ * CLI para crear un plugin automáticamente
+ * yarn run create-plugin help
+ */
+
 import path from 'path';
 import fs from 'fs';
+const LANGS = ["en", "es"];
 const BASE = "plugins/";
 let options = {
     name: "",
     displayName: "",
     category: "multimedia",
-    isReact: true,
     visor: true,
     isRich: false,
 };
@@ -15,9 +20,9 @@ function p(text) {
 }
 
 function help() {
-    p("Uso: npm run create-plugin -- \"<Nombre del plugin>\" ");
+    p("Uso: yarn run create-plugin \"<Nombre del plugin>\" <opciones>");
+    p(" o bien : npm run create-plugin -- \"<Nombre del plugin>\"  <opciones> ");
     p("  Opciones: ");
-    p("     legacy:                Plugin JS en vez de React ");
     p("     no-visor:              Plugin sin definir para el visor ");
     p("     rich:                  Plugin enriquecido ");
     p("     category <categoría>:  Categoría del plugin ");
@@ -41,9 +46,6 @@ function parseArgs(args) {
             options.displayName = val;
         } else if (index > 2) {
             switch(val) {
-            case "legacy":
-                options.isReact = false;
-                break;
             case "no-visor":
                 options.visor = false;
                 break;
@@ -63,18 +65,27 @@ function parseArgs(args) {
             }
         }
     });
-    createPlugins();
-    p("Opciones");
+    p("Opciones:");
     p(options);
+    createPlugins();
 
 }
 
 function createPlugins() {
     let dir = BASE + options.name;
-    console.log(template());
     if(!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
-        fs.writeFileSync(dir + "/options.name" + ".js", "");
+        fs.writeFileSync(dir + "/" + options.name + ".js", template());
+        fs.mkdirSync(dir + "/locales");
+        for (let l in LANGS) {
+            fs.writeFileSync(dir + "/locales/" + LANGS[l] + ".js", locales());
+        }
+        if(options.visor) {
+            fs.mkdirSync(dir + "/visor");
+            fs.writeFileSync(dir + "/visor/" + options.name + ".js", visorTemplate());
+        }
+        p("Plugin creado!");
+        p("Accede a core/config.es6 y añade " + options.name + ' a pluginList');
     } else {
         p("Ya existe un directorio con el nombre " + options.name);
         p("Escoja otro nombre para el plugin o borre el directorio existente");
@@ -84,117 +95,165 @@ function createPlugins() {
 }
 
 function template() {
-    return `
-    import React from 'react';
-    import i18n from 'i18next';
-    
-    export function ${options.name}(base) {
-        return {
-            getConfig: function() {
-                return {
-                    name: '${options.name}',
-                    displayName: i18n.t('${options.name}.PluginName'),
-                    category: ${options.category},
-                    ${options.isReact ? "flavor: 'react'," : "\b"}
-                    ${options.isRich ? "true," : "\b"}
-                    needsConfigModal: false,
-                    needsTextEdition: false,
-                    initialWidth: '480px',
-                    initialHeight: "270px",
-                    initialWidthSlide: '30%',
-                    initialHeightSlide: '30%',
-                    icon: 'label',
-                };
-            },
-            getToolbar: function() {
-                return {
-                    main: {
-                        __name: "Main",
-                        accordions: {
-    
-                            style: {
-                                __name: i18n.t('${options.name}.box_style'),
-                                icon: 'palette',
-                                buttons: {
-                                    padding: {
-                                        __name: i18n.t('${options.name}.padding'),
-                                        type: 'number',
-                                        value: 0,
-                                        min: 0,
-                                        max: 100,
-                                    },
-                                    backgroundColor: {
-                                        __name: i18n.t('${options.name}.background_color'),
-                                        type: 'color',
-                                        value: '#ffffff',
-                                    },
-                                    borderWidth: {
-                                        __name: i18n.t('${options.name}.border_size'),
-                                        type: 'number',
-                                        value: 0,
-                                        min: 0,
-                                        max: 10,
-                                    },
-                                    borderStyle: {
-                                        __name: i18n.t('${options.name}.border_style'),
-                                        type: 'select',
-                                        value: 'solid',
-                                        options: ['none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset', 'initial', 'inherit'],
-                                    },
-                                    borderColor: {
-                                        __name: i18n.t('${options.name}.border_color'),
-                                        type: 'color',
-                                        value: '#000000',
-                                    },
-                                    borderRadius: {
-                                        __name: i18n.t('${options.name}.radius'),
-                                        type: 'number',
-                                        value: 0,
-                                        min: 0,
-                                        max: 50,
-                                    },
-                                    opacity: {
-                                        __name: i18n.t('${options.name}.opacity'),
-                                        type: 'range',
-                                        value: 1,
-                                        min: 0,
-                                        max: 1,
-                                        step: 0.01,
-                                    },
-                                },
-                            },
-                            basic: {
-                                __name: i18n.t('${options.name}.name'),
-                                icon: 'link',
-                                buttons: {
-                                    url: {
-                                        __name: 'Name',
-                                        type: 'text',
-                                        value: base.getState().name,
-                                        autoManaged: false,
-                                    },
+    return `import React from 'react';
+import i18n from 'i18next';
+${options.isRich ? `
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import MarkEditor from '../../_editor/components/rich_plugins/mark_editor/MarkEditor';
+` : ''}
+export function ${options.name}(base) {
+    return {
+        getConfig: function() {
+            return {
+                name: '${options.name}',
+                displayName: i18n.t('${options.name}.PluginName'),
+                category: "${options.category}",
+                flavor: "react",
+                needsConfigModal: false,
+                needsTextEdition: false,
+                initialWidth: '480px',
+                initialHeight: "270px",
+                initialWidthSlide: '30%',
+                initialHeightSlide: '30%',
+                icon: 'label',
+                ${options.isRich ? "isRich: true," : ""}
+                ${options.isRich ? "marksType: [{ name: 'Mark', key: 'value', format: '[x,y]', default: '50,50', defaultColor: '#222222' }]," : ""}
+            };
+        },
+        getToolbar: function() {
+            return {
+                main: {
+                    __name: "Main",
+                    accordions: {
+                        basic: {
+                            __name: 'Config',
+                            icon: 'link',
+                            buttons: {
+                                name: {
+                                    __name: 'Config',
+                                    type: 'text',
+                                    value: base.getState().name,
+                                    autoManaged: false,
                                 },
                             },
                         },
                     },
-                };
-            },
-            getInitialState: function() {
-                return {
-                    name: "Ediphy",
-                };
-            },
-            getRenderTemplate: function(state) {
-                return (<div>Hello {state.name} </div>);
-    
-            },
-            handleToolbar: function(name, value) {
-                base.setState(name, value);
-            },
-            
-        };
-    }
+                },
+            };
+        },
+        getInitialState: function() {
+            return {
+                name: "Ediphy",
+            };
+        },
+        getRenderTemplate: function(state) {
+        ${options.isRich ? `
+            let marks = state.__marks || {};
+            let Mark = ({ idKey, title, style, color }) => (
+                <MarkEditor style={style} time={1.5} mark={idKey} base={base} state={state}>
+                    <OverlayTrigger key={idKey} text={title} placement="top" overlay={<Tooltip id={idKey}>{title}</Tooltip>}>
+                        <a className="mapMarker" href="#">
+                            <i key="i" style={{ color: color }} className="material-icons">room</i>
+                        </a>
+                    </OverlayTrigger>
+                </MarkEditor>);
+
+            let markElements = Object.keys(marks).map((id) =>{
+                let value = marks[id].value;
+                let title = marks[id].title;
+                let color = marks[id].color;
+
+                let position;
+                if (value && value.split(',').length === 2) {
+                    position = value.split(',');
+                } else{
+                    position = [0, 0];
+                }
+
+                return (<Mark key={id} style={{ position: 'absolute', top: position[0] + "%", left: position[1] + "%" }} color={color} idKey={id} title={title} />);
+             });
+           ` : ``}
+            return (<div style={{ height: "100%", width: "100%" }} className="dropableRichZone">
+                Hello {state.name} 
+                ${options.isRich ? '{markElements}' : ''}
+            </div>);
+
+        },
+        handleToolbar: function(name, value) {
+            base.setState(name, value);
+        },
+        ${options.isRich ? `
+        parseRichMarkInput: function(...value) {
+            let x = (value[0] + 12) * 100 / value[2];
+            let y = (value [1] + 26) * 100 / value[3];
+            let finalValue = y.toFixed(2) + "," + x.toFixed(2);
+
+            return finalValue;
+        },
+        validateValueInput: function(value) {
+            let regex = /(^-*\\d+(?:\\.\\d*)?),(-*\\d+(?:\\.\\d*)?$)/g;
+            let match = regex.exec(value);
+            if(match && match.length === 3) {
+                let x = Math.round(parseFloat(match[1]) * 100) / 100;
+                let y = Math.round(parseFloat(match[2]) * 100) / 100;
+                if (isNaN(x) || isNaN(y)/* || x > 100 || y > 100 || x < -100 || y < -100*/) {
+                    return { isWrong: true, message: "404" };
+                }
+                value = x + ',' + y;
+            } else {
+                return { isWrong: true, message: "404" };
+            }
+            return { isWrong: false, value: value };
+        },
+
+            ` : ``}
+        
+        
+    };
+}
 `;
+}
+function locales() {
+    return `
+module.exports = {
+    "${options.name}": {
+        "PluginName": "${options.displayName}"
+    },
+};`;
+}
+
+function visorTemplate() {
+    return`
+import React from 'react';
+${options.isRich ? `
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+` : ''}
+export function ${options.name}(base) {
+    ${options.isRich ? `
+    let marks = state.__marks;
+    let box_id = id;
+
+    let markElements = Object.keys(marks).map((e) =>{
+
+        let position = marks[e].value.split(',');
+        let title = marks[e].title;
+        let color = marks[e].color;
+
+        return(
+            <a key={e} style={{ position: 'absolute', top: position[0] + "%", left: position[1] + "%", width: '24px', height: '26px' }} onClick={()=>{this.onMarkClicked(box_id, marks[e].value);}} href="#">
+                <OverlayTrigger placement="top" overlay={<Tooltip positionLeft="-12" id={e}>{title}</Tooltip>}>
+                    <i key="i" style={{ width: "100%", height: "100%", position: 'absolute', top: '-26px', left: '-12px', color: color }} className="material-icons">room</i>
+                </OverlayTrigger>
+            </a>
+        );
+    });` : ''}
+    return {
+        getRenderTemplate: function(state) {
+            return <div style={{ height: "100%", width: "100%" }} >Hello {state.name}</div>
+        }
+    };
+}
+    `;
 }
 
 parseArgs(process.argv);
