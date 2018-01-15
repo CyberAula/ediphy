8a31402c638959723a4e5182fff1c1e7054a22ec
Lourdesmarco <lourdesmarco.design@gmail.com>
2018-01-15 17:55:59 +0100

    starting with pdfjs

--- index.jsx
+++ index.jsx
@@ -18,3 +18,31 @@ import './sass/style.scss';
 new sortable();
 
 ReactDOM.render((<ReduxProvider />), document.getElementById('root'));
+
+// testing pdfs
+let pdfjsLib = require('pdfjs-dist');
+let pdfPath = './dist/sample.pdf';
+// Setting worker path to worker bundle.
+pdfjsLib.PDFJS.workerSrc = '../../build/webpack/pdf.worker-bundle.js';
+
+// Loading a document.
+let loadingTask = pdfjsLib.getDocument(pdfPath);
+loadingTask.promise.then(function(pdfDocument) {
+    // Request a first page
+    return pdfDocument.getPage(1).then(function(pdfPage) {
+        // Display page on the existing canvas with 100% scale.
+        let viewport = pdfPage.getViewport(1.0);
+        let canvas = document.getElementById('maincontent');
+        canvas.width = viewport.width;
+        canvas.height = viewport.height;
+        let ctx = canvas.getContext('2d');
+        let renderTask = pdfPage.render({
+            canvasContext: ctx,
+            viewport: viewport,
+        });
+        return renderTask.promise;
+    });
+}).catch(function(reason) {
+    console.error('Error: ' + reason);
+});
+
