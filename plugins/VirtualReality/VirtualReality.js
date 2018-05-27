65ab5c2213db0f776ba28eb5f0e856f83cceab99
pagonzal13 <paulagg176@gmail.com>
2018-05-28 01:30:59 +0200

    Background and text interaction

--- plugins/VirtualReality/VirtualReality.js
+++ plugins/VirtualReality/VirtualReality.js
@@ -50,6 +50,11 @@ export function VirtualReality(base) {
 
             return (<div>
                 <button id="postmessage">Comprobar conexión con iframe</button>
+                <select name="imagen" id="imagen">
+                    <option value="" disabled selected>Elija un fondo...</option>
+                    <option value="pano-nature.jpg">Fondo de atardecer</option>
+                    <option value="pano-planets.jpg">Fondo de planetas</option>
+                </select>
                 <button id="postimgBack">ChangeImgBack</button>
                 <iframe allow="vr" width= '100%' height= '370px' src='http://localhost:8081/index.html' id="receiver"/>
 
@@ -57,21 +62,22 @@ export function VirtualReality(base) {
 
         },
         afterRender: function(element, state) {
-            // Ventada del iframe
+            // Ventana del iframe
             let receiverWindow = document.getElementById("receiver").contentWindow;
-
             // Envío de datos al iframe
             document.getElementById("postmessage").addEventListener("click", function() {
-                receiverWindow.postMessage({ conexion: "Hay conexion?" }, "http://localhost:8081/index.html");
+                receiverWindow.postMessage({ conexion: "Conexión correcta" }, "http://localhost:8081/index.html");
+                document.getElementById("postmessage").style.display = "none";
             });
             document.getElementById("postimgBack").addEventListener("click", function() {
-                receiverWindow.postMessage({ imagenBack: "Url imagen" }, "http://localhost:8081/index.html");
+                let rutaima = document.getElementById("imagen").options[document.getElementById("imagen").selectedIndex].value;
+                receiverWindow.postMessage({ imagenBack: rutaima }, "http://localhost:8081/index.html");
             });
 
             // Datos recibidos de confirmación
             window.addEventListener("message", receiveMessage, false);
             function receiveMessage(event) {
-                console.log("EDiphy: ha llegado esto: " + event.data);
+                console.log("(EDiphy) ha llegado esto: " + event.data);
             }
         },
 
