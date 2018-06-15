8dbc7368e255352e9244e0866ddf45020ce8b4b0
Lourdesmarco <lourdesmarco.design@gmail.com>
2018-06-15 11:49:10 +0200

    VirtualReality components & reciever fix

---
+++ plugins/VirtualReality/components/VirtualRealityPluginEditor.js
@@ -0,0 +1,47 @@
+import React from "react";
+import i18n from 'i18next';
+
+export default class VirtualRealityPluginEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+        this.toolbarUpdateValue = this.toolbarUpdateValue.bind(this);
+        this.receiver = this.receiver.bind(this);
+    }
+    render() {
+        return (<iframe className={'VR'} allow="vr" width= '100%' height= '100%' src={'http://localhost:8081/index.html?id=' + this.props.id} id="receiver"/>);
+    }
+    componentDidMount() {
+        window.addEventListener("message", this.receiver);
+    }
+    componentWillUnmount() {
+        window.removeEventListener('message', this.receiver);
+    }
+    receiver(e) {
+        try{
+            let data = JSON.parse(e.data);
+            console.log(data);
+            if (!this.windowSource && data.msg === 'load' && data.id === this.props.id) {
+                this.windowSource = e.source;
+                this.toolbarUpdateValue();
+            }
+        } catch (err) {
+            console.error(err);
+        }
+
+    }
+    toolbarUpdateValue(props = this.props) {
+        let receiverWindow = this.windowSource;
+        if(receiverWindow) {
+            let { imagenBack, audioBack } = props.state;
+            receiverWindow.postMessage({ imagenBack, audioBack: { play: audioBack } }, "*");
+        }
+
+    }
+    componentWillReceiveProps(nextProps) {
+        if (JSON.stringify(this.props.state) !== JSON.stringify(nextProps.state)) {
+            this.toolbarUpdateValue(nextProps);
+        }
+    }
+}
--- plugins/VirtualReality/visor/VirtualReality.js
+++ plugins/VirtualReality/visor/VirtualReality.js
@@ -1,26 +1,11 @@
 
 import React from 'react';
-
+import VirtualRealityPlugin from '../components/VirtualRealityPlugin';
 /* eslint-disable react/prop-types */
 export function VirtualReality(base) {
-
     return {
         getRenderTemplate: function(state, props) {
-
-            return (
-                <div>
-            		<iframe allow="vr" width= '100%' height= '100%' src='http://localhost:8081/index.html' id="receiver" style={{ pointerEvents: 'all' }} />);
-        		</div>
-        	);
-        },
-        afterRender: function(element, state) {
-        	console.log("Esto no sé por qué no sale");
-        	let receiverWindow = document.getElementById("receiver").contentWindow;
-
-            let rutaima = state.imagenBack;
-            let playAudio = state.audioBack;
-            receiverWindow.postMessage({ imagenBack: rutaima, audioBack: { play: playAudio } }, "http://localhost:8081/index.html");
-
+            return <VirtualRealityPlugin state={state} id={props.id} />;
         },
     };
 }
