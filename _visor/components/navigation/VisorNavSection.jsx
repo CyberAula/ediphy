6e0eb02cfc3b5fbc7f45493fd3eb6e129fdacc2c
Lourdesmarco <lourdesmarco.design@gmail.com>
2018-07-30 12:28:05 +0200

    Fix detail

--- _visor/components/navigation/VisorNavSection.jsx
+++ _visor/components/navigation/VisorNavSection.jsx
@@ -25,25 +25,25 @@ export default class VisorNavSection extends Component {
             if (!pageObj.hidden) {
                 if (isSection(page)) {
                     return (<VisorNavSection display={this.state.toggled}
-                        key={page}
-                        pageName={page}
-                        navItemSelected={this.props.navItemSelected}
-                        progress={this.props.progress}
-                        navItemsById={this.props.navItemsById}
-                        navItemsIds={this.props.navItemsIds}
-                        viewToolbars={this.props.viewToolbars}
-                        first={this.props.first} last={this.props.last}
-                        changeCurrentView={(pageNum) => {this.props.changeCurrentView(pageNum);}} />);
+                                             key={page}
+                                             pageName={page}
+                                             navItemSelected={this.props.navItemSelected}
+                                             progress={this.props.progress}
+                                             navItemsById={this.props.navItemsById}
+                                             navItemsIds={this.props.navItemsIds}
+                                             viewToolbars={this.props.viewToolbars}
+                                             first={this.props.first} last={this.props.last}
+                                             changeCurrentView={(pageNum) => {this.props.changeCurrentView(pageNum);}} />);
                 }
                 let isVisited = this.props.progress[page];
 
                 return (<li key={page} id={'nav-' + page}
-                    onClick={(e)=>{this.props.changeCurrentView(page);}}
-                    className={this.state.toggled ? "visorNavListEl" : "visorNavListEl hiddenNavVisor"}>
+                            onClick={(e)=>{this.props.changeCurrentView(page);}}
+                            className={this.state.toggled ? "visorNavListEl" : "visorNavListEl hiddenNavVisor"}>
                     <span className={"progressBall"}><ProgressBall isVisited={isVisited} isTop={page === this.props.first} isBottom={page === this.props.last} /></span>
                     <a style={{ paddingLeft: margin }}
-                        className={this.props.navItemSelected === page ? "indexElementTitle selectedNavItemVisor" : "indexElementTitle"}
-                        href="#">
+                       className={this.props.navItemSelected === page ? "indexElementTitle selectedNavItemVisor" : "indexElementTitle"}
+                       href="#">
 
                         {(this.props.navItemsById[page].customSize === 0) ?
                             <i className="material-icons">{isSlide(this.props.navItemsById[page].type) ? "slideshow" : "insert_drive_file"}</i>
@@ -58,10 +58,8 @@ export default class VisorNavSection extends Component {
         let descendants = findDescendantNavItems(this.props.navItemsById, this.props.pageName);
         let last = (descendants.indexOf(this.props.last) > -1 && !this.state.toggled) ? this.props.pageName : this.props.last;
         return (
-
             <ul className={classes} id={'nav-' + this.props.pageName}>
-                <li className="visorNavListEl">
-                    <button onClick={(e)=>{
+                <li className=" " onClick={(e)=>{
                     if (Ediphy.Config.sections_have_content) {
                         this.props.changeCurrentView(this.props.pageName);
                     } else {
@@ -71,10 +69,13 @@ export default class VisorNavSection extends Component {
                     <a className={this.props.navItemSelected === this.props.pageName ? "indexElementTitle selectedNavItemVisor" : "indexElementTitle  "} style={{ paddingLeft: marginUl }} href="#">
                         {this.state.toggled ?
                             (<i onClick={(e)=>{this.setState({ toggled: !this.state.toggled });}} className="material-icons arrowSection">keyboard_arrow_down</i>) : (<i onClick={(e)=>{this.setState({ toggled: !this.state.toggled });}} className="material-icons arrowSection">keyboard_arrow_right</i>)}
+
                         <span> {name} </span>
-                    </button>
+                    </a>
                 </li>
+
                 { childrenNavs }
+
             </ul>
         );
     }
@@ -102,23 +103,23 @@ VisorNavSection.propTypes = {
      */
     changeCurrentView: PropTypes.func.isRequired,
     /**
-   * Objects Array that contains all created views (identified by its *id*)
-   */
+     * Objects Array that contains all created views (identified by its *id*)
+     */
     navItemsIds: PropTypes.array.isRequired,
     /**
-   * First navitem visible
-   */
+     * First navitem visible
+     */
     first: PropTypes.string,
     /**
-   * Last navitem visible
-   */
+     * Last navitem visible
+     */
     last: PropTypes.string,
     /**
-   * Calculated completed pages
-   */
+     * Calculated completed pages
+     */
     progress: PropTypes.object,
     /**
-   * View toolbars
-   */
+     * View toolbars
+     */
     viewToolbars: PropTypes.object,
 };
