d68ce159c71aea756b15d92dd7b1670fdd0d00d4
Alfonso  Jiménez Martínez <aljimenez97@gmail.com>
2018-10-23 12:55:18 +0200

    panel title redesigned, arrow rotates on expand and collapse

--- _editor/components/nav_bar/export/ExportModal.jsx
+++ _editor/components/nav_bar/export/ExportModal.jsx
@@ -110,7 +110,7 @@ export default class ExportModal extends Component {
                                             <PanelGroup accordion id="accordion-uncontrolled-example" defaultActiveKey="1">
                                                 <Panel eventKey="1">
                                                     <Panel.Heading>
-                                                        <Panel.Title toggle>{i18n.t('messages.slides_and_pages')}</Panel.Title>
+                                                        <Panel.Title toggle>{i18n.t('messages.slides_and_pages')}<em className={"material-icons expandArrow"}>expand_more</em></Panel.Title>
                                                     </Panel.Heading>
                                                     <Panel.Body collapsible>
                                                         <div className={"pageTemplates"}>
@@ -143,7 +143,7 @@ export default class ExportModal extends Component {
                                                 </Panel>
                                                 <Panel eventKey="2">
                                                     <Panel.Heading>
-                                                        <Panel.Title toggle>{i18n.t('messages.only_slides')}</Panel.Title>
+                                                        <Panel.Title toggle>{i18n.t('messages.only_slides')}<em className={"material-icons expandArrow"}>expand_more</em></Panel.Title>
                                                     </Panel.Heading>
                                                     <Panel.Body collapsible>
                                                         <div className={"pageTemplates"}>
@@ -182,7 +182,7 @@ export default class ExportModal extends Component {
                                                 </Panel>
                                                 <Panel eventKey="3">
                                                     <Panel.Heading>
-                                                        <Panel.Title toggle>{i18n.t('messages.only_pages')}</Panel.Title>
+                                                        <Panel.Title toggle>{i18n.t('messages.only_pages')}<em className={"material-icons expandArrow"}>expand_more</em></Panel.Title>
                                                     </Panel.Heading>
                                                     <Panel.Body collapsible>
                                                         <div className={"pageTemplates"}>
