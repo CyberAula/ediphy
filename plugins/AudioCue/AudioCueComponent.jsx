ec34ca3466b41608ba3d88ce3f3aa4749079572f
Alfonso  Jiménez Martínez <aljimenez97@gmail.com>
2018-11-06 11:49:42 +0100

    Some Interface Changes and animations

--- plugins/AudioCue/AudioCueComponent.jsx
+++ plugins/AudioCue/AudioCueComponent.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 /* eslint-disable react/prop-types */
 require('./AudioCue.scss');
+import imagePlay from "./../../dist/images/play.svg";
+import imagePause from "./../../dist/images/pause.svg";
 
 export default class AudioCueComponent extends React.Component {
     constructor(props) {
@@ -10,68 +12,77 @@ export default class AudioCueComponent extends React.Component {
             autoplay: this.props.state.autoplay,
             icon: this.props.state.icon,
             allowDeformed: this.props.state.allowDeformed,
+            playing: false,
         };
-        this.audio = new Audio();
+        this.audio = new Audio(props.url);
+    }
+    playPause() {
+        console.log('[INFO] this.state.playing is: ' + this.state.playing);
+        if (this.state.playing) {
+            console.log();
+            // this.audio.pause();
+            console.log('[INFO] Audio pausado');
+        } else {
+            // console.log(this.audio.play());
+            console.log('[INFO] Audio iniciado');
+        }
+        this.setState({ playing: !this.state.playing });
+    }
+    // If deleted while playing, audio should be stopped
+    componentWillUnmount() {
+        this.audio.pause();
     }
     render() {
         let { props, state } = this.props;
-        let audio = this.audio;
-        audio.setAttribute('src', state.url);
-        audio.load();
-        let playing = false;
-        if (state.autoplay) {
-            audio.autoplay = true;
-            playing = true;
-            console.log('[INFO] AutoPlay ON. Audio iniciado.');
+        this.audio.setAttribute('src', state.url);
+        // this.audio.load();
+        let imagePlayPause = this.state.playing ? imagePause : imagePlay;
+        if(this.state.playing) {
+            this.audio.play();
+            console.log('[INFO] Audio iniciado');
+        } else {
+            this.audio.pause();
+            console.log('[INFO] Audio pausado');
         }
-        let clickAudio = function() {
-            playing = !playing;
-            let loader = document.getElementsByClassName('loader');
-            loader[0].style.visibility = playing ? "visible" : "hidden";
 
-            if(playing) {
-                audio.play();
-                console.log('[INFO] Audio iniciado');
-            } else {
-                audio.pause();
-                console.log('[INFO] Audio pausado');
-            }
-        };
         return(
             <div>
-                {/* <div className="loader" onClick={clickAudio} style={{ visibility: "hidden", position: "absolute" }}>
-                    <svg id="wave" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 38.05">
-                        <title>Audio Wave</title>
-                        <path id="Line_1" data-name="Line 1" d="M0.91,15L0.78,15A1,1,0,0,0,0,16v6a1,1,0,1,0,2,0s0,0,0,0V16a1,1,0,0,0-1-1H0.91Z"/>
-                        <path id="Line_2" data-name="Line 2" d="M6.91,9L6.78,9A1,1,0,0,0,6,10V28a1,1,0,1,0,2,0s0,0,0,0V10A1,1,0,0,0,7,9H6.91Z"/>
-                        <path id="Line_3" data-name="Line 3" d="M12.91,0L12.78,0A1,1,0,0,0,12,1V37a1,1,0,1,0,2,0s0,0,0,0V1a1,1,0,0,0-1-1H12.91Z"/>
-                        <path id="Line_4" data-name="Line 4" d="M18.91,10l-0.12,0A1,1,0,0,0,18,11V27a1,1,0,1,0,2,0s0,0,0,0V11a1,1,0,0,0-1-1H18.91Z"/>
-                        <path id="Line_5" data-name="Line 5" d="M24.91,15l-0.12,0A1,1,0,0,0,24,16v6a1,1,0,0,0,2,0s0,0,0,0V16a1,1,0,0,0-1-1H24.91Z"/>
-                        <path id="Line_6" data-name="Line 6" d="M30.91,10l-0.12,0A1,1,0,0,0,30,11V27a1,1,0,1,0,2,0s0,0,0,0V11a1,1,0,0,0-1-1H30.91Z"/>
-                        <path id="Line_7" data-name="Line 7" d="M36.91,0L36.78,0A1,1,0,0,0,36,1V37a1,1,0,1,0,2,0s0,0,0,0V1a1,1,0,0,0-1-1H36.91Z"/>
-                        <path id="Line_8" data-name="Line 8" d="M42.91,9L42.78,9A1,1,0,0,0,42,10V28a1,1,0,1,0,2,0s0,0,0,0V10a1,1,0,0,0-1-1H42.91Z"/>
-                        <path id="Line_9" data-name="Line 9" d="M48.91,15l-0.12,0A1,1,0,0,0,48,16v6a1,1,0,1,0,2,0s0,0,0,0V16a1,1,0,0,0-1-1H48.91Z"/>
-                    </svg>
-                </div>*/}
-
-                <div className={"loader"} id="bars" onClick={clickAudio} style={{ visibility: "hidden", position: "absolute" }}>
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
+                <div className={"loader"} id="bars" onClick={this.playPause.bind(this)} style={{ visibility: this.state.playing ? "visible" : "hidden", position: "absolute" }}>
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
 
                 </div>
-                <button onClick={clickAudio} style={{ height: "100%", width: "100%", pointerEvents: "initial" }} className={"draggableImage"} ref={"draggableImage"}>
+                <button onClick={this.playPause.bind(this)} style={{ height: "100%", width: "100%", pointerEvents: "initial" }} className={"draggableImage"} ref={"draggableImage"}>
+                    <img className={"playButton"} src={imagePlayPause} />
                     <img ref ="img"
                         className="basicImageClass"
-                        style={{ width: state.allowDeformed ? "100%" : "100%", height: state.allowDeformed ? "" : "auto" }}
+                        style={{ width: state.allowDeformed ? "100%" : "100%", height: state.allowDeformed ? "" : "auto", visibility: "hidden" }}
                         src={state.icon}
                     />
                 </button>
--- plugins/AudioCue/visor/AudioCueComponent.jsx
+++ plugins/AudioCue/visor/AudioCueComponent.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 require('../AudioCue.scss');
 
+import imagePlay from "./../../../dist/images/play.svg";
+import imagePause from "./../../../dist/images/pause.svg";
+
 /* eslint-disable react/prop-types */
 
 export default class AudioCueComponent extends React.Component {
@@ -11,81 +14,74 @@ export default class AudioCueComponent extends React.Component {
             autoplay: this.props.state.autoplay,
             icon: this.props.state.icon,
             allowDeformed: this.props.state.allowDeformed,
+            playing: false,
         };
-        this.audio = new Audio();
+        this.audio = new Audio(props.url);
     }
+    playPause() {
+        this.setState({ playing: !this.state.playing });
+    }
+
+    componentWillMount() {
+        if(this.state.autoplay) {
+            this.setState({ playing: true });
+        }
+    }
+    componentWillUnmount() {
+        this.audio.pause();
+    }
+
     render() {
         let { props, state } = this.props;
-        let audio = this.audio;
-        audio.setAttribute('src', state.url);
-        audio.load();
-        let playing = false;
-        if (state.autoplay) {
-            audio.autoplay = true;
-            playing = true;
-            console.log('[INFO] AutoPlay ON. Audio iniciado.');
-        }
-        let clickAudio = function() {
-            playing = !playing;
-            let loader = document.getElementsByClassName('loader');
-            loader[0].style.visibility = playing ? "visible" : "hidden";
-            // playing ? console.log(audio.play()) : audio.pause();
+        this.audio.setAttribute('src', state.url);
+        // this.audio.load();
 
-            if(playing) {
-                audio.play();
-                console.log('[INFO] Audio iniciado');
-            } else {
-                audio.pause();
-                console.log('[INFO] Audio pausado');
-            }
-        };
+        let imagePlayPause = this.state.playing ? imagePause : imagePlay;
+
+        if(this.state.playing) {
+            this.audio.play();
+        } else {
+            this.audio.pause();
+        }
         return(
             <div>
-                {/* <div className="loader" onClick={clickAudio} style={{ visibility: "hidden", position: "absolute" }}>
-                    <svg id="wave" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 38.05">
-                        <title>Audio Wave</title>
-                        <path id="Line_1" data-name="Line 1" d="M0.91,15L0.78,15A1,1,0,0,0,0,16v6a1,1,0,1,0,2,0s0,0,0,0V16a1,1,0,0,0-1-1H0.91Z"/>
-                        <path id="Line_2" data-name="Line 2" d="M6.91,9L6.78,9A1,1,0,0,0,6,10V28a1,1,0,1,0,2,0s0,0,0,0V10A1,1,0,0,0,7,9H6.91Z"/>
-                        <path id="Line_3" data-name="Line 3" d="M12.91,0L12.78,0A1,1,0,0,0,12,1V37a1,1,0,1,0,2,0s0,0,0,0V1a1,1,0,0,0-1-1H12.91Z"/>
-                        <path id="Line_4" data-name="Line 4" d="M18.91,10l-0.12,0A1,1,0,0,0,18,11V27a1,1,0,1,0,2,0s0,0,0,0V11a1,1,0,0,0-1-1H18.91Z"/>
-                        <path id="Line_5" data-name="Line 5" d="M24.91,15l-0.12,0A1,1,0,0,0,24,16v6a1,1,0,0,0,2,0s0,0,0,0V16a1,1,0,0,0-1-1H24.91Z"/>
-                        <path id="Line_6" data-name="Line 6" d="M30.91,10l-0.12,0A1,1,0,0,0,30,11V27a1,1,0,1,0,2,0s0,0,0,0V11a1,1,0,0,0-1-1H30.91Z"/>
-                        <path id="Line_7" data-name="Line 7" d="M36.91,0L36.78,0A1,1,0,0,0,36,1V37a1,1,0,1,0,2,0s0,0,0,0V1a1,1,0,0,0-1-1H36.91Z"/>
-                        <path id="Line_8" data-name="Line 8" d="M42.91,9L42.78,9A1,1,0,0,0,42,10V28a1,1,0,1,0,2,0s0,0,0,0V10a1,1,0,0,0-1-1H42.91Z"/>
-                        <path id="Line_9" data-name="Line 9" d="M48.91,15l-0.12,0A1,1,0,0,0,48,16v6a1,1,0,1,0,2,0s0,0,0,0V16a1,1,0,0,0-1-1H48.91Z"/>
-                    </svg>
-                </div>*/}
-
-                <div className={"loader"} id="bars" onClick={clickAudio} style={{ visibility: "hidden", position: "absolute" }}>
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
-                    <div className="bar" />
+                <div className={"loader"} id="bars" onClick={this.playPause.bind(this)} style={{ visibility: this.state.playing ? "visible" : "hidden", position: "absolute" }}>
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barUp" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
+                    <div className="barDown" />
 
                 </div>
 
-                <button onClick={clickAudio} style={{ height: "100%", width: "100%", pointerEvents: "initial" }} className={"draggableImage"} ref={"draggableImage"}>
+                <button onClick={this.playPause.bind(this)} style={{ height: "100%", width: "100%", pointerEvents: "initial" }} className={"draggableImage"} ref={"draggableImage"}>
+                    <img className={"playButton"} src={imagePlayPause} />
                     <img ref ="img"
                         className="basicImageClass"
-                        style={{ width: state.allowDeformed ? "100%" : "100%", height: state.allowDeformed ? "" : "auto" }}
+                        style={{ width: state.allowDeformed ? "100%" : "100%", height: state.allowDeformed ? "" : "100%" }}
                         src={state.icon}
                     />
                 </button>
