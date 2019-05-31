79a6bc42b5a30a827da0772d936b162bde60b56b
Ana Marin <ana.marin.e@gmail.com>
2019-05-31 13:55:01 +0200

    polygon templates in progress

--- common/themes/polygon/polygon.js
+++ common/themes/polygon/polygon.js
@@ -45,10 +45,145 @@ export const DEFINITION = {
         themeColor7: '#DF0000',
     },
     images: {
+        template0: { background: 2 },
         template1: { background: 1, left: 'left.jpg' },
         template2: { background: 2 },
         template3: { background: 2, topLeft: 'topLeft.png', topRight: 'topRight.png', bottomLeft: 'bottomLeft.png', bottomRight: 'bottomRight.png' },
         template7: { left: 'seven.jpg' },
         template12: { background: 3 },
     },
+    templates: [
+        {
+            "name": 'display',
+            "boxes": [
+                {
+                    "box": {
+                        "x": "5%",
+                        "y": "6%",
+                        "width": "90%",
+                        "height": "12%",
+                    },
+                    "toolbar": {
+                        "name": "BasicText",
+                        "style": { "color": 'var(--themeColor1)' },
+                        "text": "<h1 class='no_margins'> Cabecera </h1>",
+                    },
+                    "thumbnail": {
+                        "icon": "",
+                        "icon_color": "",
+                        "color": 'var(--themeColor1)',
+                    },
+                },
+                {
+                    "box": {
+                        "x": "10%",
+                        "y": "22%",
+                        "width": "22%",
+                        "height": "40%",
+                    },
+                    "toolbar": {
+                        "name": "HotspotImages",
+                        "url": `/themes/polygon/seven.jpg`,
+                        "style": { "clipPath": "polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)" },
+                    },
+                    "thumbnail": {
+                        "icon": "filter_hdr",
+                        "icon_color": "#90E1EC",
+                        "color": "#43BECE",
+                    },
+                },
+                {
+                    "box": {
+                        "x": "39%",
+                        "y": "22%",
+                        "width": "22%",
+                        "height": "40%",
+                    },
+                    "toolbar": {
+                        "name": "HotspotImages",
+                        "url": `/themes/polygon/seven.jpg`,
+                        "style": { "clipPath": "polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)" },
+                    },
+                    "thumbnail": {
+                        "icon": "filter_hdr",
+                        "icon_color": "#90E1EC",
+                        "color": "#43BECE",
+                    },
+                },
+                {
+                    "box": {
+                        "x": "68%",
+                        "y": "22%",
+                        "width": "22%",
+                        "height": "40%",
+                    },
+                    "toolbar": {
+                        "name": "HotspotImages",
+                        "url": `/themes/polygon/seven.jpg`,
+                        "style": { "clipPath": "polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)" },
+                    },
+                    "thumbnail": {
+                        "icon": "filter_hdr",
+                        "icon_color": "#90E1EC",
+                        "color": "#43BECE",
+                    },
+                },
+                {
+                    "box": {
+                        "x": "19.5%",
+                        "y": "62%",
+                        "width": "3%",
+                        "height": "10%",
+                    },
+                    "toolbar": {
+                        "name": "HotspotImages",
+                        "url": `/themes/polygon/arrow.png`,
+
+                    },
+                    "thumbnail": {
+                        "icon": "",
+                        "icon_color": "",
+                        "color": '',
+                    },
+                },
+                {
+                    "box": {
+                        "x": "48.5%",
+                        "y": "62%",
+                        "width": "3%",
+                        "height": "10%",
+                    },
+                    "toolbar": {
+                        "name": "HotspotImages",
+                        "url": `/themes/polygon/arrow.png`,
+
+                    },
+                    "thumbnail": {
+                        "icon": "",
+                        "icon_color": "",
+                        "color": '',
+                    },
+                },
+                {
+                    "box": {
+                        "x": "77.5%",
+                        "y": "62%",
+                        "width": "3%",
+                        "height": "10%",
+                    },
+                    "toolbar": {
+                        "name": "HotspotImages",
+                        "url": `/themes/polygon/arrow.png`,
+
+                    },
+                    "thumbnail": {
+                        "icon": "",
+                        "icon_color": "",
+                        "color": '',
+                    },
+                },
+
+            ],
+        },
+    ],
 };
