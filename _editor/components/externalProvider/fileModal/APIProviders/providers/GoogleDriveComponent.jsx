f2265d8fdecca48d134145276ee8fd3a7fa7f0d9
Alfonso JM <aljimenez97@gmail.com>
2019-09-09 11:46:08 +0200

    (ref) GoogleDriveComponent.jsx: removed unused-vars

--- _editor/components/externalProvider/fileModal/APIProviders/providers/GoogleDriveComponent.jsx
+++ _editor/components/externalProvider/fileModal/APIProviders/providers/GoogleDriveComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Modal, FormControl, Col, Form, FormGroup, InputGroup, Glyphicon, ControlLabel, Button } from 'react-bootstrap';
+import { Col, Form, FormGroup, Button } from 'react-bootstrap';
 import i18n from 'i18next';
 import GooglePicker from 'react-google-picker';
 export default class GoogleDriveComponent extends React.Component {
@@ -34,8 +34,8 @@ export default class GoogleDriveComponent extends React.Component {
                 <GooglePicker clientId={'854542748878-51e02udhsu2q2cp512fg7kalbplkim6j.apps.googleusercontent.com'}
                     developerKey={'AIzaSyDbKOqlWXUmWCRcjtY4gSBld8Qspey-yRY'}
                     scope={['https://www.googleapis.com/auth/drive.readonly']}
-                    onChange={data => console.log('on change:', data)}
-                    onAuthFailed={data => console.log('on auth failed:', data)}
+                    onChange={data => data /* console.log('on change:', data)*/}
+                    onAuthFailed={data => data /* console.log('on auth failed:', data)*/}
                     multiselect={false}
                     navHidden={false}
                     authImmediate={false}
@@ -50,14 +50,14 @@ export default class GoogleDriveComponent extends React.Component {
 }
 
 GoogleDriveComponent.propTypes = {
-    /**
-     * Selected Element
-     */
-    elementSelected: PropTypes.any,
-    /**
-     * Select element callback
-     */
-    onElementSelected: PropTypes.func.isRequired,
+    // /**
+    //  * Selected Element
+    //  */
+    // elementSelected: PropTypes.any,
+    // /**
+    //  * Select element callback
+    //  */
+    // onElementSelected: PropTypes.func.isRequired,
     /**
      * Icon that identifies the API provider
      */
