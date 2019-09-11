f1fa8c9b5acd126791b41020ff3ede8eb8ffd3ff
Alfonso JM <aljimenez97@gmail.com>
2019-09-11 09:16:20 +0200

    (ref) handleContainedViews.js: export arrow function

--- _editor/handlers/handleContainedViews.js
+++ _editor/handlers/handleContainedViews.js
@@ -4,19 +4,17 @@ import {
 } from "../../common/actions";
 import { getDescendantBoxes } from "../../common/utils";
 
-export default function(self) {
-    return {
-        onContainedViewNameChanged: (id, titleStr) => self.props.dispatch(updateViewToolbar(id, titleStr)),
+export default (self) => ({
+    onContainedViewNameChanged: (id, titleStr) => self.props.dispatch(updateViewToolbar(id, titleStr)),
 
-        onContainedViewSelected: (id) => self.props.dispatch(selectContainedView(id)),
+    onContainedViewSelected: (id) => self.props.dispatch(selectContainedView(id)),
 
-        onContainedViewDeleted: (cvid) => {
-            let boxesRemoving = [];
-            self.props.containedViews[cvid].boxes.map(boxId => {
-                boxesRemoving.push(boxId);
-                boxesRemoving = boxesRemoving.concat(getDescendantBoxes(self.props.boxes[boxId], self.props.boxes));
-            });
-            self.props.dispatch(deleteContainedView([cvid], boxesRemoving, self.props.containedViews[cvid].parent));
-        },
-    };
-}
+    onContainedViewDeleted: (cvid) => {
+        let boxesRemoving = [];
+        self.props.containedViews[cvid].boxes.map(boxId => {
+            boxesRemoving.push(boxId);
+            boxesRemoving = boxesRemoving.concat(getDescendantBoxes(self.props.boxes[boxId], self.props.boxes));
+        });
+        self.props.dispatch(deleteContainedView([cvid], boxesRemoving, self.props.containedViews[cvid].parent));
+    },
+});
