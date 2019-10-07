5d68aec014b8e3b586d610b6f3f61a3cadb771a3
Alfonso JM <aljimenez97@gmail.com>
2019-10-07 08:45:59 +0200

    (ref) utils.es6: getBrowser function moved to utils

--- _editor/containers/ErrorBoundary.jsx
+++ _editor/containers/ErrorBoundary.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import ErrorStackParser from 'error-stack-parser';
 import Ediphy from '../../core/editor/main';
 import PropTypes from "prop-types";
+import { get_browser } from "../../common/utils";
 
 class ErrorBoundary extends React.Component {
     state = { hasError: false };
@@ -95,24 +96,6 @@ class ErrorBoundary extends React.Component {
     }
 }
 
-function get_browser() {
-    let ua = navigator.userAgent, tem, M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    if((/trident/i).test(M[1])) {
-        tem = (/\brv[ :]+(\d+)/g).exec(ua) || [];
-        return { name: 'IE', version: (tem[1] || '') };
-    }
-    if(M[1] === 'Chrome') {
-        tem = ua.match(/\bOPR|Edge\/(\d+)/);
-        if(tem !== null) {return { name: 'Opera', version: tem[1] };}
-    }
-    M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
-    if((tem = ua.match(/version\/(\d+)/i)) !== null) {M.splice(1, 1, tem[1]);}
-    return {
-        name: M[0],
-        version: M[1],
-    };
-}
-
 function mapStateToProps(state) {
     return {
         state: state,
@@ -139,4 +122,8 @@ ErrorBoundary.propTypes = {
      * React children
      */
     children: PropTypes.any,
+    /**
+     * Name of the plugin throwing the error
+     */
+    pluginName: PropTypes.str,
 };
